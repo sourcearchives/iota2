9e6ddc3c0e7f4e2f87d5466f2749ccf87c126884
Jordi Inglada <jordi.inglada@cesbio.cnes.fr>
2016-02-16 21:37:54 +0100

    TEST: infra for tests

---
+++ test/iota2FeatureExtractionFunctor.cxx
@@ -0,0 +1,20 @@
+/*=========================================================================
+
+  Program:   gapfilling
+  Language:  C++
+
+  Copyright (c) Jordi Inglada. All rights reserved.
+
+  See LICENSE for details.
+
+  This software is distributed WITHOUT ANY WARRANTY; without even
+  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+  PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
+int fexFunctor(int itkNotUsed(argc), char * itkNotUsed(argv)[])
+{
+
+  return EXIT_SUCCESS;
+}
