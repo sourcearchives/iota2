305cc497b55c5c726b07bb00d48ee6cfed84ef0d
Arthur VINCENT <vincenta@cesbio.cnes.fr>
2016-03-17 13:17:04 +0000

    bug v5.2.1 OTB -> remettre le buildType lorsque bug résolu

--- scripts/common/genJobLaunchFeat.py
+++ scripts/common/genJobLaunchFeat.py
@@ -2,11 +2,18 @@
 #-*- coding: utf-8 -*-
 
 import argparse,os
-
+from config import Config
 
 #############################################################################################################################
 
-def genJob(jobPath,testPath,logPath):
+def genJob(jobPath,testPath,logPath,pathConf):
+
+	f = file(pathConf)
+	cfg = Config(f)
+
+	OTB_VERSION = cfg.chain.OTB_version
+	OTB_BUILDTYPE = cfg.chain.OTB_buildType
+	OTB_INSTALLDIR = cfg.chain.OTB_installDir
 
 	pathToJob = jobPath+"/extractfeatures.pbs"
 	if os.path.exists(pathToJob):
@@ -39,6 +46,16 @@ module remove xerces/2.7\n\
 module load xerces/2.8\n\
 module load gdal/1.11.0-py2.7\n\
 \n\
+pkg="otb_superbuild"\n\
+version="%s"\n\
+build_type="%s"\n\
+name=$pkg-$version\n\
+install_dir=%s/$pkg/$name-install/\n\
+\n\
+export ITK_AUTOLOAD_PATH=""\n\
+export PATH=$install_dir/bin:$PATH\n\
+export LD_LIBRARY_PATH=$install_dir/lib:$install_dir/lib/otb/python:${LD_LIBRARY_PATH}\n\
+\n\
 j=0\n\
 old_IFS=$IFS\n\
 IFS=$\'%s\'\n\
@@ -49,7 +66,7 @@ do\n\
 done\n\
 IFS=$old_IFS\n\
 \n\
-eval ${cmd[${PBS_ARRAY_INDEX}]}'%(Ncmd-1,logPath,logPath,'\\n'))
+eval ${cmd[${PBS_ARRAY_INDEX}]}'%(Ncmd-1,logPath,logPath,OTB_VERSION,OTB_BUILDTYPE,OTB_INSTALLDIR,'\\n'))
 		jobFile.close()
 	elif Ncmd==1:
 		jobFile = open(pathToJob,"w")
@@ -71,6 +88,16 @@ module remove xerces/2.7\n\
 module load xerces/2.8\n\
 module load gdal/1.11.0-py2.7\n\
 \n\
+pkg="otb_superbuild"\n\
+version="%s"\n\
+build_type="%s"\n\
+name=$pkg-$version\n\
+install_dir=%s/$pkg/$name-install/\n\
+\n\
+export ITK_AUTOLOAD_PATH=""\n\
+export PATH=$install_dir/bin:$PATH\n\
+export LD_LIBRARY_PATH=$install_dir/lib:$install_dir/lib/otb/python:${LD_LIBRARY_PATH}\n\
+\n\
 j=0\n\
 old_IFS=$IFS\n\
 IFS=$\'%s\'\n\
@@ -81,7 +108,7 @@ do\n\
 done\n\
 IFS=$old_IFS\n\
 \n\
-eval ${cmd[0]}'%(logPath,logPath,'\\n'))
+eval ${cmd[0]}'%(logPath,logPath,OTB_VERSION,OTB_BUILDTYPE,OTB_INSTALLDIR,'\\n'))
 		jobFile.close()
 if __name__ == "__main__":
 
@@ -89,9 +116,10 @@ if __name__ == "__main__":
 	parser.add_argument("-path.job",help ="path where are all jobs (mandatory)",dest = "jobPath",required=True)
 	parser.add_argument("-path.test",help ="path to the folder which contains the test (mandatory)",dest = "testPath",required=True)
 	parser.add_argument("-path.log",help ="path to the log folder (mandatory)",dest = "logPath",required=True)
+	parser.add_argument("-conf",help ="path to the configuration file which describe the learning method (mandatory)",dest = "pathConf",required=True)
 	args = parser.parse_args()
 
-	genJob(args.jobPath,args.testPath,args.logPath)
+	genJob(args.jobPath,args.testPath,args.logPath,args.pathConf)
 
 
 
--- scripts/common/genJobLaunchTrain.py
+++ scripts/common/genJobLaunchTrain.py
@@ -2,11 +2,18 @@
 #-*- coding: utf-8 -*-
 
 import argparse,os
-
+from config import Config
 
 #############################################################################################################################
 
-def genJob(jobPath,testPath,logPath):
+def genJob(jobPath,testPath,logPath,pathConf):
+
+	f = file(pathConf)
+	cfg = Config(f)
+
+	OTB_VERSION = cfg.chain.OTB_version
+	OTB_BUILDTYPE = cfg.chain.OTB_buildType
+	OTB_INSTALLDIR = cfg.chain.OTB_installDir
 
 	pathToJob = jobPath+"/launchTrain.pbs"
 	if os.path.exists(pathToJob):
@@ -28,11 +35,20 @@ def genJob(jobPath,testPath,logPath):
 #PBS -o %s/LaunchTrain_out.log\n\
 #PBS -e %s/LaunchTrain_err.log\n\
 \n\
-\n\
 module load python/2.7.5\n\
 module remove xerces/2.7\n\
 module load xerces/2.8\n\
 module load gdal/1.11.0-py2.7\n\
+\n\
+pkg="otb_superbuild"\n\
+version="%s"\n\
+build_type="%s"\n\
+name=$pkg-$version\n\
+install_dir=%s/$pkg/$name-install/\n\
+\n\
+export ITK_AUTOLOAD_PATH=""\n\
+export PATH=$install_dir/bin:$PATH\n\
+export LD_LIBRARY_PATH=$install_dir/lib:$install_dir/lib/otb/python:${LD_LIBRARY_PATH}\n\
 \n\
 j=0\n\
 old_IFS=$IFS\n\
@@ -48,7 +64,7 @@ echo ${cmd[${PBS_ARRAY_INDEX}]}\n\
 eval ${cmd[${PBS_ARRAY_INDEX}]}\n\
 #dataCp=($(find $TMPDIR -maxdepth 1 -type f -name "model*.txt"))\n\
 #cp ${dataCp[0]} $TESTPATH/model\n\
-'%(Ncmd-1,logPath,logPath,'\\n'))
+'%(Ncmd-1,logPath,logPath,OTB_VERSION,OTB_BUILDTYPE,OTB_INSTALLDIR,'\\n'))
 
 		jobFile.close()
 	elif Ncmd == 1:
@@ -66,6 +82,16 @@ module remove xerces/2.7\n\
 module load xerces/2.8\n\
 module load gdal/1.11.0-py2.7\n\
 \n\
+pkg="otb_superbuild"\n\
+version="%s"\n\
+build_type="%s"\n\
+name=$pkg-$version\n\
+install_dir=%s/$pkg/$name-install/\n\
+\n\
+export ITK_AUTOLOAD_PATH=""\n\
+export PATH=$install_dir/bin:$PATH\n\
+export LD_LIBRARY_PATH=$install_dir/lib:$install_dir/lib/otb/python:${LD_LIBRARY_PATH}\n\
+\n\
 j=0\n\
 old_IFS=$IFS\n\
 IFS=$\'%s\'\n\
@@ -79,7 +105,7 @@ IFS=$old_IFS\n\
 echo ${cmd[${PBS_ARRAY_INDEX}]}\n\
 eval ${cmd[0]}\n\
 #dataCp=($(find $TMPDIR -maxdepth 1 -type f -name "model*.txt"))\n\
-#cp ${dataCp[0]} $TESTPATH/model'%(logPath,logPath,'\\n'))
+#cp ${dataCp[0]} $TESTPATH/model'%(logPath,logPath,OTB_VERSION,OTB_BUILDTYPE,OTB_INSTALLDIR,'\\n'))
 
 		jobFile.close()
 if __name__ == "__main__":
@@ -88,9 +114,10 @@ if __name__ == "__main__":
 	parser.add_argument("-path.job",help ="path where are all jobs (mandatory)",dest = "jobPath",required=True)
 	parser.add_argument("-path.test",help ="path to the folder which contains the test (mandatory)",dest = "testPath",required=True)
 	parser.add_argument("-path.log",help ="path to the log folder (mandatory)",dest = "logPath",required=True)	
+	parser.add_argument("-conf",help ="path to the configuration file which describe the learning method (mandatory)",dest = "pathConf",required=True)	
 	args = parser.parse_args()
 
-	genJob(args.jobPath,args.testPath,args.logPath)
+	genJob(args.jobPath,args.testPath,args.logPath,args.pathConf)
 
 
 
