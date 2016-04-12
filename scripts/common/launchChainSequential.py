2c6c85d80b86a94ae946e46e7aaceebe50a85e03
Jordi Inglada <jordi.inglada@cesbio.cnes.fr>
2016-04-12 11:02:44 +0200

    REFAC: tranform generated file into python code

---
+++ scripts/common/launchChainSequential.py
@@ -0,0 +1,162 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+import tileEnvelope as env
+import tileArea as area
+import LaunchTraining as LT
+import createRegionsByTiles as RT
+import ExtractDataByRegion as ExtDR
+import RandomInSituByTile as RIST
+import launchClassification as LC
+import ClassificationShaping as CS
+import genConfusionMatrix as GCM
+import ModelStat as MS
+import genResults as GR
+import genCmdFeatures as GFD
+import os
+import fusion as FUS
+import noData as ND
+import confusionFusion as confFus
+import reArrangeModel as RAM
+
+def launchChainSequential(PathTEST, tiles, pathTilesL8, pathTilesL5,pathNewProcessingChain, pathTilesFeat, configFeature, shapeRegion, field_Region, model, shapeData, dataField, pathConf, N, REARRANGE_PATH):
+    os.system("rm -r "+PathTEST)
+    fieldEnv = "FID"#do not change
+
+    pathModels = PathTEST+"/model"
+    pathEnvelope = PathTEST+"/envelope"
+    pathClassif = PathTEST+"/classif"
+    pathTileRegion = PathTEST+"/shapeRegion"
+    classifFinal = PathTEST+"/final"
+    dataRegion = PathTEST+"/dataRegion"
+    pathAppVal = PathTEST+"/dataAppVal"
+    pathStats = PathTEST+"/stats"
+    cmdPath = PathTEST+"/cmd"
+
+    if not os.path.exists(PathTEST):
+        os.system("mkdir "+PathTEST)
+    if not os.path.exists(pathModels):
+        os.system("mkdir "+pathModels)
+    if not os.path.exists(pathEnvelope):
+        os.system("mkdir "+pathEnvelope)
+    if not os.path.exists(pathClassif):
+        os.system("mkdir "+pathClassif)
+    if not os.path.exists(pathTileRegion):
+        os.system("mkdir "+pathTileRegion)
+    if not os.path.exists(classifFinal):
+        os.system("mkdir "+classifFinal)
+    if not os.path.exists(dataRegion):
+        os.system("mkdir "+dataRegion)
+    if not os.path.exists(pathAppVal):
+        os.system("mkdir "+pathAppVal)
+    if not os.path.exists(pathStats):
+        os.system("mkdir "+pathStats)
+    if not os.path.exists(cmdPath):
+        os.system("mkdir "+cmdPath)
+        os.system("mkdir "+cmdPath+"/stats")
+        os.system("mkdir "+cmdPath+"/train")
+        os.system("mkdir "+cmdPath+"/cla")
+        os.system("mkdir "+cmdPath+"/confusion")
+        os.system("mkdir "+cmdPath+"/features")
+        os.system("mkdir "+cmdPath+"/fusion")
+
+    feat = GFD.CmdFeatures(PathTEST,tiles,pathNewProcessingChain,pathTilesL8,pathTilesL5,pathConf,pathTilesFeat,None)
+    for i in range(len(feat)):
+        print feat[i]
+        os.system(feat[i])
+
+    #Création des enveloppes
+    env.GenerateShapeTile(tiles,pathTilesFeat,pathEnvelope,None,configFeature)
+
+
+    if MODE != "outside":
+        area.generateRegionShape(MODE,pathEnvelope,model,shapeRegion,field_Region,None)
+
+    #Création des régions par tuiles
+    RT.createRegionsByTiles(shapeRegion,field_Region,pathEnvelope,pathTileRegion,None)
+
+    #pour tout les fichiers dans pathTileRegion
+    regionTile = RT.FileSearch_AND(pathTileRegion,".shp")
+    #/////////////////////////////////////////////////////////////////////////////////////////
+    for path in regionTile:
+        ExtDR.ExtractData(path,shapeData,dataRegion,pathTilesFeat,None)
+        #/////////////////////////////////////////////////////////////////////////////////////////
+
+    if REARRANGE_FLAG :
+        RAM.generateRepartition(PathTEST,pathConf,shapeRegion,REARRANGE_PATH,dataField)
+        #pour tout les shape file par tuiles présent dans dataRegion, créer un ensemble dapp et de val
+    dataTile = RT.FileSearch_AND(dataRegion,".shp")
+    #/////////////////////////////////////////////////////////////////////////////////////////
+    for path in dataTile:
+        RIST.RandomInSituByTile(path,dataField,N,pathAppVal,None)
+        #/////////////////////////////////////////////////////////////////////////////////////////
+
+    #/////////////////////////////////////////////////////////////////////////////////////////
+    #génération des fichiers de statistiques
+    AllCmd = MS.generateStatModel(pathAppVal,pathTilesFeat,pathStats,cmdPath+"/stats",None,configFeature)
+
+    for cmd in AllCmd:
+        print cmd
+        print ""
+        os.system(cmd)
+        #/////////////////////////////////////////////////////////////////////////////////////////
+
+    #génération des commandes pour lApp
+    allCmd = LT.launchTraining(pathAppVal,pathConf,pathTilesFeat,dataField,pathStats,N,cmdPath+"/train",pathModels,None,None)
+    #/////////////////////////////////////////////////////////////////////////////////////////
+    for cmd in allCmd:
+        print cmd
+        print ""
+        os.system(cmd)
+        #/////////////////////////////////////////////////////////////////////////////////////////
+
+
+    #génération des commandes pour la classification
+    cmdClassif = LC.launchClassification(pathModels,pathConf,pathStats,pathTileRegion,pathTilesFeat,shapeRegion,field_Region,N,cmdPath+"/cla",pathClassif,None)
+    #/////////////////////////////////////////////////////////////////////////////////////////
+    for cmd in cmdClassif:
+        print cmd 
+        print ""
+        os.system(cmd)
+        #/////////////////////////////////////////////////////////////////////////////////////////
+
+    if CLASSIFMODE == "separate":
+        #Mise en forme des classifications
+        CS.ClassificationShaping(pathClassif,pathEnvelope,pathTilesFeat,fieldEnv,N,classifFinal,None,configFeature)
+
+        #génération des commandes pour les matrices de confusions
+        allCmd_conf = GCM.genConfMatrix(classifFinal,pathAppVal,N,dataField,cmdPath+"/confusion",configFeature,None)
+            for cmd in allCmd_conf:
+                print cmd
+                os.system(cmd)
+
+        confFus.confFusion(shapeData,dataField,classifFinal+"/TMP",classifFinal+"/TMP",classifFinal+"/TMP",configFeature)
+        GR.genResults(classifFinal,NOMENCLATURE)
+    elif CLASSIFMODE == "fusion" and MODE != "one_region":
+        cmdFus = FUS.fusion(pathClassif,configFeature,None)
+        for cmd in cmdFus:
+            print cmd
+            os.system(cmd)
+
+        #gestion des nodata
+        fusionFiles = RT.FileSearch_AND(pathClassif,"_FUSION_seed_")
+        for fusionpath in fusionFiles:
+            ND.noData(PathTEST,fusionpath,field_Region,pathTilesFeat,shapeRegion,N,None)
+
+        #Mise en forme des classifications
+        CS.ClassificationShaping(pathClassif,pathEnvelope,pathTilesFeat,fieldEnv,N,classifFinal,None,configFeature)
+
+        #génération des commandes pour les matrices de confusions
+        allCmd_conf = GCM.genConfMatrix(classifFinal,pathAppVal,N,dataField,cmdPath+"/confusion",configFeature,None)
+        #/////////////////////////////////////////////////////////////////////////////////////////
+        for cmd in allCmd_conf:
+            print cmd
+            os.system(cmd)
+            #/////////////////////////////////////////////////////////////////////////////////////////
+
+        confFus.confFusion(shapeData,dataField,classifFinal+"/TMP",classifFinal+"/TMP",classifFinal+"/TMP",configFeature)
+        GR.genResults(classifFinal,NOMENCLATURE)
+
+    elif CLASSIFMODE == "fusion" and MODE =="one_region":
+        raise Exception("You can't choose the 'one region' mode and use the fusion mode together")
+
