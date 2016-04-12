ced9193dab5b4ef8fb5d2e4d9e87e1ba9f2a096b
Jordi Inglada <jordi.inglada@cesbio.cnes.fr>
2016-04-12 09:47:51 +0200

    STYLE: typo

--- scripts/common/launchChain.sh
+++ scripts/common/launchChain.sh
@@ -17,13 +17,13 @@ IFS=$old_IFS
 
 if [[ $cmd{[0]} == *Red* ]]
 then
-	echo "Server detected : parallel chain will be launch"
+	echo "Server detected : parallel chain will be launched"
 	module load python/2.7.5
 	module remove xerces/2.7
 	module load xerces/2.8
 else
 	echo $cmd
-	echo "Local mode sequential chain will be launch"
+	echo "Local mode sequential chain will be launched"
 fi
 
 python launchChain.py -launch.config $1
