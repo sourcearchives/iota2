b128d1116ade392fc57cbde79d84080737a7139f
Arthur VINCENT <vincenta@cesbio.cnes.fr>
2016-08-16 07:09:22 +0000

    ENH : add vectorSamplesMerge.py

---
+++ scripts/common/vectorSamplesMerge.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+# =========================================================================
+#   Program:   iota2
+#
+#   Copyright (c) CESBIO. All rights reserved.
+#
+#   See LICENSE for details.
+#
+#   This software is distributed WITHOUT ANY WARRANTY; without even
+#   the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+#   PURPOSE.  See the above copyright notices for more information.
+#
+# =========================================================================
+
+import argparse,os
+import fileUtils as fu
+from config import Config
+
+def getAllModelsFromShape(PathLearningSamples):
+	AllSample = fu.fileSearchRegEx(PathLearningSamples+"/*.shp")
+	AllModels = []
+	for currentSample in AllSample:
+		try:
+			model = currentSample.split("/")[-1].split("_")[-4]
+			ind = AllModels.index(model)
+		except ValueError:
+			AllModels.append(model)
+	return AllModels
+
+def vectorSamplesMerge(pathConf):
+	
+	f = file(pathConf)
+	cfg = Config(f)
+	outputPath = cfg.chain.outputPath
+	runs = int(cfg.chain.runs)
+
+	AllModels = getAllModelsFromShape(outputPath+"/learningSamples")
+	
+	for seed in range(runs):
+		for currentModel in AllModels:
+			learningShapes = fu.fileSearchRegEx(outputPath+"/learningSamples/*_region_"+currentModel+"_seed"+str(seed)+"*.shp")
+			shapeOut = "Samples_region_"+currentModel+"_seed"+str(seed)+"_learn"
+			folderOut = outputPath+"/learningSamples"
+			fu.mergeVectors(shapeOut, folderOut,learningShapes)
+			for currentShape in learningShapes:
+				fu.removeShape(currentShape.replace(".shp",""),[".prj",".shp",".dbf",".shx"])
+
+if __name__ == "__main__":
+
+	parser = argparse.ArgumentParser(description = "This function creates the jobArray.pbs for training")	
+	parser.add_argument("-conf",help ="path to the configuration file (mandatory)",dest = "pathConf",required=True)	
+	args = parser.parse_args()
+
+	vectorSamplesMerge(args.pathConf)
