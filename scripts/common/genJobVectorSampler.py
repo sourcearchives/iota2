6af6ff372f5072fc1fd5a358095a1d904472f4df
Arthur VINCENT <vincenta@cesbio.cnes.fr>
2016-11-02 08:27:01 +0000

    REFAC : use OTB given in configuration file instead of force OTB's version in genJobVectorSampler.py

--- scripts/common/genJobVectorSampler.py
+++ scripts/common/genJobVectorSampler.py
@@ -20,22 +20,22 @@ import fileUtils as fu
 
 def genJob(jobPath,testPath,logPath,pathConf):
 
-	f = file(pathConf)
-	cfg = Config(f)
+    f = file(pathConf)
+    cfg = Config(f)
 
-	pathToJob = jobPath+"/vectorSampler.pbs"
-	if os.path.exists(pathToJob):
-		os.system("rm "+pathToJob)
+    pathToJob = jobPath+"/vectorSampler.pbs"
+    if os.path.exists(pathToJob):
+        os.system("rm "+pathToJob)
 
-	AllTrainShape = fu.FileSearch_AND(testPath+"/dataAppVal",True,"learn.shp")
-	nbShape = len(AllTrainShape)
+    AllTrainShape = fu.FileSearch_AND(testPath+"/dataAppVal",True,"learn.shp")
+    nbShape = len(AllTrainShape)
 
-	if nbShape>1:
-		jobFile = open(pathToJob,"w")
-		jobFile.write('#!/bin/bash\n\
+    if nbShape>1:
+        jobFile = open(pathToJob,"w")
+        jobFile.write('#!/bin/bash\n\
 #PBS -N vectorSampler\n\
 #PBS -J 0-%s:1\n\
-#PBS -l select=1:ncpus=1:mem=20000mb\n\
+#PBS -l select=1:ncpus=1:mem=10000mb\n\
 #PBS -m be\n\
 #PBS -l walltime=03:00:00\n\
 \n\
@@ -46,7 +46,8 @@ module load xerces/2.8\n\
 module load gdal/1.11.0-py2.7\n\
 \n\
 FileConfig=%s\n\
-. /data/qtis/inglada/modules/repository/otb_superbuild/otb_superbuild-5.7.0-Release-install/config_otb.sh\n\
+export OTB_HOME=$(grep --only-matching --perl-regex "^((?!#).)*(?<=OTB_HOME\:).*" $FileConfig | cut -d "\'" -f 2)\n\
+. $OTB_HOME/config_otb.sh\n\
 export ITK_GLOBAL_DEFAULT_NUMBER_OF_THREADS=1\n\
 \n\
 PYPATH=$(grep --only-matching --perl-regex "^((?!#).)*(?<=pyAppPath\:).*" $FileConfig | cut -d "\'" -f 2)\n\
@@ -57,12 +58,12 @@ listData=($(find $TESTPATH/dataAppVal -maxdepth 1 -type f -name "*learn.shp"))\n
 InShape=${listData[${PBS_ARRAY_INDEX}]}\n\
 echo $InShape\n\
 python vectorSampler.py -shape $InShape -conf $FileConfig --wd $TMPDIR'%(nbShape-1,pathConf))
-		jobFile.close()
-	else:
-		jobFile = open(pathToJob,"w")
-		jobFile.write('#!/bin/bash\n\
+        jobFile.close()
+    else:
+        jobFile = open(pathToJob,"w")
+        jobFile.write('#!/bin/bash\n\
 #PBS -N vectorSampler\n\
-#PBS -l select=1:ncpus=1:mem=20000mb\n\
+#PBS -l select=1:ncpus=5:mem=20000mb\n\
 #PBS -m be\n\
 #PBS -l walltime=03:00:00\n\
 #PBS -o %s/vectorSampler_out.log\n\
@@ -78,7 +79,7 @@ FileConfig=%s\n\
 export ITK_AUTOLOAD_PATH=""\n\
 . /data/qtis/inglada/modules/repository/otb_superbuild/otb_superbuild-5.7.0-Release-install/config_otb.sh\n\
 \n\
-export ITK_GLOBAL_DEFAULT_NUMBER_OF_THREADS=1\n\
+export ITK_GLOBAL_DEFAULT_NUMBER_OF_THREADS=5\n\
 PYPATH=$(grep --only-matching --perl-regex "^((?!#).)*(?<=pyAppPath\:).*" $FileConfig | cut -d "\'" -f 2)\n\
 TESTPATH=$(grep --only-matching --perl-regex "^((?!#).)*(?<=outputPath\:).*" $FileConfig | cut -d "\'" -f 2)\n\
 cd $PYPATH\n\
@@ -86,18 +87,18 @@ cd $PYPATH\n\
 listData=($(find $TESTPATH/dataAppVal -maxdepth 1 -type f -name "*learn.shp"))\n\
 InShape=${listData[0]}\n\
 python vectorSampler.py -shape $InShape -conf $FileConfig --wd $TMPDIR'%(logPath,logPath,pathConf))
-		jobFile.close()
+        jobFile.close()
 
 if __name__ == "__main__":
 
-	parser = argparse.ArgumentParser(description = "This function creates the jobArray.pbs for DataAppVal")
-	parser.add_argument("-path.job",help ="path where are all jobs (mandatory)",dest = "jobPath",required=True)
-	parser.add_argument("-path.test",help ="path to the folder which contains the test (mandatory)",dest = "testPath",required=True)
-	parser.add_argument("-path.log",help ="path to the log folder (mandatory)",dest = "logPath",required=True)
-	parser.add_argument("-conf",help ="path to the configuration file which describe the learning method (mandatory)",dest = "pathConf",required=True)
-	args = parser.parse_args()
+    parser = argparse.ArgumentParser(description = "This function creates the jobArray.pbs for DataAppVal")
+    parser.add_argument("-path.job",help ="path where are all jobs (mandatory)",dest = "jobPath",required=True)
+    parser.add_argument("-path.test",help ="path to the folder which contains the test (mandatory)",dest = "testPath",required=True)
+    parser.add_argument("-path.log",help ="path to the log folder (mandatory)",dest = "logPath",required=True)
+    parser.add_argument("-conf",help ="path to the configuration file which describe the learning method (mandatory)",dest = "pathConf",required=True)
+    args = parser.parse_args()
 
-	genJob(args.jobPath,args.testPath,args.logPath,args.pathConf)
+    genJob(args.jobPath,args.testPath,args.logPath,args.pathConf)
 
 
 
