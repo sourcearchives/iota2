fa7f3c7cd4351cc37972f9f71c0f7f99d948b917
Arthur VINCENT <vincenta@cesbio.cnes.fr>
2017-05-19 11:30:25 +0200

    ENH add datas needed to run tests

--- scripts/common/iota2tests.py
+++ scripts/common/iota2tests.py
@@ -22,16 +22,15 @@ from osgeo import gdal
 from config import Config
 import numpy as np
 
-#export IOTA2_dataTest=/mnt/data/home/vincenta/IOTA2/test_data
-#export PYTHONPATH=$PYTHONPATH:/mnt/data/home/vincenta/modulePy/config-0.3.9
-#export PYTHONPATH=$PYTHONPATH:/mnt/data/home/vincenta/IOTA2/test_data/test_scripts
-#export IOTA2DIR=/mnt/data/home/vincenta/IOTA2/theia_oso
+#export PYTHONPATH=$PYTHONPATH:/mnt/data/home/vincenta/modulePy/config-0.3.9       -> get python Module
+#export PYTHONPATH=$PYTHONPATH:/mnt/data/home/vincenta/theia_oso/data/test_scripts -> get scripts needed to test
+#export IOTA2DIR=/mnt/data/home/vincenta/theia_oso
 
-#python -m unittest iota2tests.iota_testSeq
+#python -m unittest iota2tests
 
 iota2dir = os.environ.get('IOTA2DIR')
 iota2_script = os.environ.get('IOTA2DIR')+"/scripts/common"
-iota2_dataTest = os.environ.get('IOTA2_dataTest')
+iota2_dataTest = os.environ.get('IOTA2DIR')+"/data/"
 
 def rasterToArray(InRaster):
         arrayOut = None
@@ -189,6 +188,9 @@ class iota_testShapeManipulations(unittest.TestCase):
                 self.priorityEnvelope_ref = iota2_dataTest+"/references/priority_ref"
                 self.splitRatio = 0.5               
 
+		self.test_vector = iota2_dataTest+"/test_vector"
+                if os.path.exists(self.test_vector):shutil.rmtree(self.test_vector)
+                os.mkdir(self.test_vector)     
                 
 
 	def test_CountFeatures(self):
