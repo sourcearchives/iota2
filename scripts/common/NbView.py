8aef9502202fca9ee644389ed5ca5097893fa50b
cedric penard <cedric.penard@capgemini.com>
2017-07-26 15:49:26 +0200

    BUG: bug correction in computeNbView

--- scripts/common/NbView.py
+++ scripts/common/NbView.py
@@ -23,44 +23,46 @@ from config import Config
 from vectorSampler import gapFillingToSample
 
 def buildExpression_cloud(Path_Mask):
-	
-	ds = gdal.Open(Path_Mask, GA_ReadOnly)
-	bands = ds.RasterCount
 
-	exp = "-".join(["im1b"+str(band+1) for band in range(bands)])
-	return str(bands)+"-"+exp
+    ds = gdal.Open(Path_Mask, GA_ReadOnly)
+    bands = ds.RasterCount
+
+    exp = "-".join(["im1b"+str(band+1) for band in range(bands)])
+    return str(bands)+"-"+exp
 	
 def getLineNumberInFiles(fileList):
-        
+
     nbLine = 0
-    for currentFile in fileList :
+    for currentFile in fileList:
         with open(currentFile,'r') as currentF:
-                for line in currentF:
-                        nbLine+=1
+            for line in currentF:
+                nbLine+=1
     return nbLine
 
 def computeNbView(tile,workingDirectory,pathConf,outputRaster,tilePath):
     
     import serviceConfigFile as SCF
     # load configuration file
-    cfg = SCF.serviceConfigFile(args.pathConf)
+    cfg = SCF.serviceConfigFile(pathConf)
     
     print "Computing pixel validity by tile"
     tilesStackDirectory = workingDirectory+"/"+tile+"_STACK"
-    if not os.path.exists(tilesStackDirectory) : os.mkdir(tilesStackDirectory)
+    if not os.path.exists(tilesStackDirectory):
+        os.mkdir(tilesStackDirectory)
     AllRefl,AllMask,datesInterp,realDates = gapFillingToSample("trainShape","samplesOptions",\
                                                                tilesStackDirectory,"samples",\
                                                                "dataField",tilesStackDirectory,tile,\
                                                                cfg, wMode=False,onlySensorsMasks=True)
     
-    if not os.path.exists(tilePath+"/tmp") : 
-	os.mkdir(tilePath+"/tmp")
-	fu.updateDirectory(tilesStackDirectory+"/"+tile+"/tmp",tilePath+"/tmp")
-    if not os.path.exists(tilePath+"/Final") :
-	os.mkdir(tilePath+"/Final")
-	fu.updateDirectory(tilesStackDirectory+"/"+tile+"/Final",tilePath+"/Final")
+    if not os.path.exists(tilePath+"/tmp"): 
+        os.mkdir(tilePath+"/tmp")
+        fu.updateDirectory(tilesStackDirectory+"/"+tile+"/tmp",tilePath+"/tmp")
+    if not os.path.exists(tilePath+"/Final"):
+        os.mkdir(tilePath+"/Final")
+        fu.updateDirectory(tilesStackDirectory+"/"+tile+"/Final",tilePath+"/Final")
 
-    for currentMask in AllMask : currentMask[0].Execute()
+    for currentMask in AllMask:
+        currentMask[0].Execute()
     concat = fu.CreateConcatenateImagesApplication(AllMask,pixType='uint8',wMode=False,output="")
     concat.Execute()
     nbRealDates = getLineNumberInFiles(realDates)
@@ -71,39 +73,34 @@ def computeNbView(tile,workingDirectory,pathConf,outputRaster,tilePath):
     return tilesStackDirectory
 
 def genNbView(TilePath,maskOut,nbview,pathConf,workingDirectory = None):
-	"""
-	"""
-        allTiles = (Config(file(pathConf)).chain.listTile).split()
-        tile = fu.findCurrentTileInString(TilePath,allTiles)
-
-	nameNbView = "nbView.tif"
-	wd = TilePath
-	if workingDirectory:wd = workingDirectory
-	tilePixVal = wd+"/"+nameNbView
-
-        if not os.path.exists(TilePath):os.mkdir(TilePath)
-
-	if not os.path.exists(TilePath+"/"+nameNbView):
-
-		tmp2 = maskOut.replace(".shp","_tmp_2.tif").replace(TilePath,wd)
-                tilesStackDirectory = computeNbView(tile,wd,pathConf,tilePixVal,TilePath)
-		cmd = 'otbcli_BandMath -il '+tilePixVal+' -out '+tmp2+' -exp "im1b1>='+str(nbview)+'?1:0"'
-		print cmd
-		os.system(cmd)
-	
-		maskOut_tmp = maskOut.replace(".shp","_tmp.shp").replace(TilePath,wd)
-		cmd = "gdal_polygonize.py -mask "+tmp2+" "+tmp2+" -f \"ESRI Shapefile\" "+maskOut_tmp
-		print cmd
-		os.system(cmd)
-		fu.erodeShapeFile(maskOut_tmp,wd+"/"+maskOut.split("/")[-1],0.1)
-	
-		os.remove(tmp2)
-		fu.removeShape(maskOut_tmp.replace(".shp",""),[".prj",".shp",".dbf",".shx"])
-	
-		if workingDirectory:
-			shutil.copy(tilePixVal,TilePath)
-			fu.cpShapeFile(wd+"/"+maskOut.split("/")[-1].replace(".shp",""),TilePath,[".prj",".shp",".dbf",".shx"],spe=True)
-                shutil.rmtree(tilesStackDirectory)
+    """
+    """
+    allTiles = (Config(file(pathConf)).chain.listTile).split()
+    tile = fu.findCurrentTileInString(TilePath,allTiles)
+    nameNbView = "nbView.tif"
+    wd = TilePath
+    if workingDirectory:
+        wd = workingDirectory
+    tilePixVal = wd+"/"+nameNbView
+    if not os.path.exists(TilePath):
+        os.mkdir(TilePath)
+    if not os.path.exists(TilePath+"/"+nameNbView):
+        tmp2 = maskOut.replace(".shp","_tmp_2.tif").replace(TilePath,wd)
+        tilesStackDirectory = computeNbView(tile,wd,pathConf,tilePixVal,TilePath)
+        cmd = 'otbcli_BandMath -il '+tilePixVal+' -out '+tmp2+' -exp "im1b1>='+str(nbview)+'?1:0"'
+        print cmd
+        os.system(cmd)
+        maskOut_tmp = maskOut.replace(".shp","_tmp.shp").replace(TilePath,wd)
+        cmd = "gdal_polygonize.py -mask "+tmp2+" "+tmp2+" -f \"ESRI Shapefile\" "+maskOut_tmp
+        print cmd
+        os.system(cmd)
+        fu.erodeShapeFile(maskOut_tmp,wd+"/"+maskOut.split("/")[-1],0.1)
+        os.remove(tmp2)
+        fu.removeShape(maskOut_tmp.replace(".shp",""),[".prj",".shp",".dbf",".shx"])
+        if workingDirectory:
+            shutil.copy(tilePixVal,TilePath)
+            fu.cpShapeFile(wd+"/"+maskOut.split("/")[-1].replace(".shp",""),TilePath,[".prj",".shp",".dbf",".shx"],spe=True)
+            shutil.rmtree(tilesStackDirectory)
 
 if __name__ == "__main__":
 
