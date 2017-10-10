66de3d084354f74d5a13754fc85be5345224298b
Jordi Inglada <jordi.inglada@cesbio.cnes.fr>
2017-10-10 11:39:45 +0200

    ENH: implement modes of reduction

--- scripts/common/DimensionalityReduction.py
+++ scripts/common/DimensionalityReduction.py
@@ -17,46 +17,84 @@
 import argparse
 import fileUtils as fu
 import serviceConfigFile as SCF
+import otbApplication as otb
 
-def CheckFieldCoherency(inputSampleFileName, numberOfDates, numberOfBandsPerDate, numberOfIndices, numberOfMetaDataFields):
+def CheckFieldCoherency(inputSampleFileName, numberOfDates, numberOfBandsPerDate, 
+                        numberOfIndices, numberOfMetaDataFields):
     """
-    Verify that the input sample file contains the right number of fields according to the number of dates, bands, indices and metadata fields.
-    """
-    nbFields = fu.getAllFieldsInShape(inputSampleFileName, 'SQLite')
-    if nbFields != numberOfDates*(numberOfBandsPerDate+numberOfIndices)+numberOfMetaDataFields:
-        raise RuntimeError("The number of fields in "+inputSampleFileName+" does not match")
 
-def BuildFeaturesLists(inputSampleFileName, numberOfDates, numberOfBandsPerDate, numberOfIndices, numberOfMetaDataFields, reductionMode):
+    Verify that the input sample file contains the right number of
+    fields according to the number of dates, bands, indices and
+    metadata fields.
+
     """
-    Build a list of lists of the features containing the features to be used for each reduction.
+    nbFields = len(fu.getAllFieldsInShape(inputSampleFileName, 'SQLite'))
+    if nbFields != (numberOfDates*(numberOfBandsPerDate+numberOfIndices)+
+                    numberOfMetaDataFields):
+        raise RuntimeError("The number of fields ("+str(nbFields)+
+                           ") in "+inputSampleFileName+" does not match")
+
+def BuildFeaturesLists(inputSampleFileName, numberOfDates, numberOfBandsPerDate, 
+                       numberOfIndices, numberOfMetaDataFields, 
+                       reductionMode='global'):
     """
-    allFeatures = ["band_"+str(x) for x in range(numberOfDates*(numberOfBandsPerDate+numberOfIndices)+numberOfMetaDataFields)]	
+    Build a list of lists of the features containing the features to be
+    used for each reduction.
+
+    'global' reduction mode selects all the features
+    'band' reduction mode selects all the dates for each feature
+    'date' reduction mode selects all the features for each date
+    """
+    allFeatures = fu.getAllFieldsInShape(inputSampleFileName, 
+                                         'SQLite')[numberOfMetaDataFields:]
     if reductionMode == 'global':
         return list(allFeatures)
     if reductionMode == 'band':
-        raise RuntimeError("band reduction mode not implemented")
+        fl = list()
+        for b in range(numberOfBandsPerDate):
+            lb = list()
+            for d in range(numberOfDates):
+                lb.append('value_'+str(numberOfBandsPerDate*d+b))
+            fl.append(lb)
+        for i in range(numberOfIndices):
+            li = list()
+            for d in range(numberOfDates):
+                li.append('value_'+str(numberOfDates*numberOfBandsPerDate+
+                                       numberOfDates*i+d))
+            fl.append(li)
+        return fl
     if reductionMode == 'date':
-        raise RuntimeError("date reduction mode not implemented")
+        fl = list()
+        for d in range(numberOfDates):
+            ld = list()
+            for b in range(numberOfBandsPerDate):
+                ld.append('value_'+str(d*numberOfBandsPerDate+b))
+            for i in range(numberOfIndices):
+                ld.append('value_'+str(numberOfDates*numberOfBandsPerDate+
+                                       numberOfDates*i+d))
+            fl.append(ld)
+        return fl
     else:
         raise RuntimeError("Unknown reduction mode")
 
     
-def TrainDimensionalityReduction(param, Model_file):
-	feat = ["band_"+str(x) for x in param['bands']]	
-	DRTrain = otb.Registry.CreateApplication("CbDimensionalityReductionTrainer")
-	DRTrain.SetParameterString("io.vd",param['Training Sample'])
-	DRTrain.SetParameterStringList("feat",feat)
-	if param['Image Stats'] is not None:
-		print param['Image Stats']
-		DRTrain.SetParameterString("io.stats",param['Image Stats'])
-	DRTrain.SetParameterString("io.out",Model_file )
+def TrainDimensionalityReduction(inputSampleFileName, outputModelFileName, 
+                                 featureList, targetDimension, statsFile = None):
+
+	DRTrain = otb.Registry.CreateApplication("TrainDimensionalityReduction")
+	DRTrain.SetParameterString("io.vd",inputSampleFileName)
+	DRTrain.SetParameterStringList("feat",featureList)
+	if statsFile is not None:
+	    DRTrain.SetParameterString("io.stats",statsFile)
+	DRTrain.SetParameterString("io.out", outputModelFileName)
 	DRTrain.SetParameterString("model","pca")
-	DRTrain.SetParameterString("model.pca.dim",param['Dimension'])
+	DRTrain.SetParameterString("model.pca.dim", targetDimension)
 	DRTrain.ExecuteAndWriteOutput()
 
-def SampleFilePCAReduction(inputSampleFileName, outputSampleFileName, reductionMode, numberOfDates, numberOfBandsPerDate, numberOfIndices, numberOfMetaDataFields):
-    """
-    usage : Apply a PCA reduction 
+def SampleFilePCAReduction(inputSampleFileName, outputSampleFileName, 
+                           reductionMode, numberOfDates, numberOfBandsPerDate, 
+                           numberOfIndices, numberOfMetaDataFields):
+    """usage : Apply a PCA reduction 
 
     IN:
     inputSampleFileName [string] : path to a vector file containing training samples
@@ -66,25 +104,34 @@ def SampleFilePCAReduction(inputSampleFileName, outputSampleFileName, reductionM
     numberOfIndices [int] : additional features added at the end
     numberOfMetaDataFields [int] : initial attributes like area, land cover type, etc.
 
-    the fields of each sample are supposed to follow this pattern (assuming N metadata fields, K bands, L dates and M additional features:
+    the fields of each sample are supposed to follow this pattern
+    (assuming N metadata fields, K bands, L dates and M additional
+    features:
 
     meta1, ..., metaN d1b1 d1b2 ... d1bK d2b1 ... dLbK f1d1 ... f1dK f2d1 ... fMdK
 
     OUT:
     outputSampleFileName [string] : name of the resulting reduced sample file
+
     """
 
-    CheckFieldCoherency(inputSampleFileName, numberOfDates, numberOfBandsPerDate, numberOfIndices, numberOfMetaDataFields)
-    featureList = BuildFeaturesLists(inputSampleFileName, numberOfDates, numberOfBandsPerDate, numberOfIndices, numberOfMetaDataFields, reductionMode)
+    CheckFieldCoherency(inputSampleFileName, numberOfDates, numberOfBandsPerDate, 
+                        numberOfIndices, numberOfMetaDataFields)
+    featureList = BuildFeaturesLists(inputSampleFileName, numberOfDates, 
+                                     numberOfBandsPerDate, numberOfIndices, 
+                                     numberOfMetaDataFields, reductionMode)
     for l in featureList:
         TrainDimensionalityReduction(inputSampleFileName)
         
 if __name__ == "__main__":
 
-    parser = argparse.ArgumentParser(description="Apply dimensionality reduction to a sample file")
-    parser.add_argument("-in", dest="inSampleFile", help="path to the input sample file",
+    parser = argparse.ArgumentParser(description=
+                                     "Apply dimensionality reduction to a sample file")
+    parser.add_argument("-in", dest="inSampleFile", 
+                        help="path to the input sample file",
                         default=None, required=True)
-    parser.add_argument("-out", dest="outSampleFile", help="path to the output sample file",
+    parser.add_argument("-out", dest="outSampleFile", 
+                        help="path to the output sample file",
                         default=None, required=True)
     parser.add_argument("-conf", help="path to the configuration file (mandatory)",
                         dest="pathConf", required=True)
