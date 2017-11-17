0b929eeea13df17962823f04718fcfb2270b558b
Arthur VINCENT <vincenta@cesbio.cnes.fr>
2017-11-17 08:43:58 +0100

    STYLE : vectorSamplesMerge.py -> pylint and PEP8

--- scripts/common/vectorSamplesMerge.py
+++ scripts/common/vectorSamplesMerge.py
@@ -26,39 +26,39 @@ def cleanRepo(outputPath):
     """
     remove from the directory learningSamples all unnecessary files
     """
-    LearningContent = os.listdir(outputPath+"/learningSamples")
+    LearningContent = os.listdir(outputPath + "/learningSamples")
     for c_content in LearningContent:
-        c_path = outputPath+"/learningSamples/"+c_content
+        c_path = outputPath + "/learningSamples/" + c_content
         if os.path.isdir(c_path):
             shutil.rmtree(c_path)
 
 
-def vectorSamplesMerge(cfg,vectorList):
+def vectorSamplesMerge(cfg, vectorList):
 
     regions_position = 2
     seed_position = 3
-    
-    if not isinstance(cfg,SCF.serviceConfigFile):
+
+    if not isinstance(cfg, SCF.serviceConfigFile):
         cfg = SCF.serviceConfigFile(cfg)
 
     outputPath = cfg.getParam('chain', 'outputPath')
     cleanRepo(outputPath)
-    
+
     currentModel = os.path.split(vectorList[0])[-1].split("_")[regions_position]
-    seed = os.path.split(vectorList[0])[-1].split("_")[seed_position].replace("seed","")
+    seed = os.path.split(vectorList[0])[-1].split("_")[seed_position].replace("seed", "")
 
     shapeOut_name = "Samples_region_" + currentModel + "_seed" + str(seed) + "_learn.sqlite"
     shapeOut_path = os.path.join(outputPath, "learningSamples", shapeOut_name)
     fu.mergeSqlite(vectorList, shapeOut_path)
-    
+
     for vector in vectorList:
         os.remove(vector)
 
 
 if __name__ == "__main__":
 
-    parser = argparse.ArgumentParser(description = "This function merge sqlite to feed training")
-    parser.add_argument("-conf", help ="path to the configuration file (mandatory)",
+    parser = argparse.ArgumentParser(description="This function merge sqlite to perform training step")
+    parser.add_argument("-conf", help="path to the configuration file (mandatory)",
                         dest="pathConf", required=True)
 
     args = parser.parse_args()
