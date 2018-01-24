fc9c2cbcf1cf9b953e043f08ec905c74c26d6260
Vincent Thierion <vincent.thierion@cesbio.cnes.fr>
2018-01-24 14:44:15 +0100

    ENH: Check if geom is not None

--- scripts/vector-tools/RandomSelectionPolygonsAreaThreshold.py
+++ scripts/vector-tools/RandomSelectionPolygonsAreaThreshold.py
@@ -31,7 +31,7 @@ def get_randomPolyAreaThresh(shapefile, field, classe, thresh, outShapefile):
     try:
         indfield = fieldList.index(field)
     except:
-        print "The field {} does not exist in the input shapefile".format(fieldin)
+        print "The field {} does not exist in the input shapefile".format(field)
         print "You must choose one of these existing fields : {}".format(' / '.join(fieldList))
         sys.exit(-1)
         
@@ -49,7 +49,8 @@ def get_randomPolyAreaThresh(shapefile, field, classe, thresh, outShapefile):
     listid = []
     for feat in layer:
         geom = feat.GetGeometryRef()
-        listid.append([feat.GetFID(), geom.GetArea()])
+        if geom is not None:
+            listid.append([feat.GetFID(), geom.GetArea()])
 
     print "Random selection"
     # random selection based on area sum threshold        
