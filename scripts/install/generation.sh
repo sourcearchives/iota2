b7d2eb120cc7a9879732f0610d38e9a09f496920
penard <Cedric PENARD>
2018-03-09 15:21:19 +0100

    ENH: improve installation script

--- scripts/install/generation.sh
+++ scripts/install/generation.sh
@@ -30,6 +30,7 @@ RED='\033[0;31m'
 # On prend comme répertoire de référence le répertoire où se trouve le script.
 prefix_dir=$PWD
 ok=0
+OTB_VERSION='6.4'
 
 # Error test
 if [[ "$#" != "1" ]] && [[ "$#" != "2" ]]; then
@@ -56,20 +57,28 @@ if [[ "$ok" == "1" ]]; then
       echo "Cloning OTB ..."
       mkdir -p $prefix_dir/OTB
       cd $prefix_dir/OTB
-      git clone https://git@git.orfeo-toolbox.org/git/otb.git
+      if [ -d "./otb" ]; then
+        echo "otb repository already cloned. skipping."
+      else
+        git clone https://git@git.orfeo-toolbox.org/git/otb.git
+      fi
 
       echo "Getting Superbuild archives ..."
       cd $prefix_dir/OTB
       mkdir -p SuperBuild-archives
       cd SuperBuild-archives
-      wget https://www.orfeo-toolbox.org/packages/SuperBuild-archives-6.4.tar.bz2
-      wget https://www.orfeo-toolbox.org/packages/SuperBuild-archives-6.4.md5
-      md5sum SuperBuild-archives-6.4.tar.bz2 > verif_MD5
-      nbDiff=`diff verif_MD5 SuperBuild-archives-6.4.md5 | wc -l`
-      if [[ "$nbDiff" == 0 ]]; then
-        tar -xvjf SuperBuild-archives-6.4.tar.bz2
+      if [ -f "./SuperBuild-archives-${OTB_VERSION}.tar.bz2" ]; then
+        echo "SuperBuild archives already downloaded. skipping."
       else
-        echo "MD5sum is different for SuperBuild-archives-6.4.tar.bz2 file !"
+        wget https://www.orfeo-toolbox.org/packages/SuperBuild-archives-${OTB_VERSION}.tar.bz2
+      fi
+      wget https://www.orfeo-toolbox.org/packages/SuperBuild-archives-${OTB_VERSION}.md5
+      md5sum SuperBuild-archives-${OTB_VERSION}.tar.bz2 > verif_MD5
+      nbDiff=`diff verif_MD5 SuperBuild-archives-${OTB_VERSION}.md5 | wc -l`
+      if [[ "$nbDiff" == 0 ]]; then
+        tar -xvjf SuperBuild-archives-${OTB_VERSION}.tar.bz2
+      else
+        echo "MD5sum is different for SuperBuild-archives-${OTB_VERSION}.tar.bz2 file !"
         echo "Maybe a problem during download ?"
         exit
       fi
@@ -79,16 +88,24 @@ if [[ "$ok" == "1" ]]; then
       echo "Adding OTBGapFilling module ..."
       mkdir -p $prefix_dir/CESBIO
       cd $prefix_dir/CESBIO
-      git clone http://tully.ups-tlse.fr/jordi/temporalgapfilling.git
+      if [ -d "./temporalgapfilling" ]; then
+        echo "temporalgapfilling repository already cloned. skipping."
+      else
+        git clone http://tully.ups-tlse.fr/jordi/temporalgapfilling.git
+      fi
       cd $prefix_dir/OTB/otb/Modules/Remote/
-      ln -s ../../../../CESBIO/temporalgapfilling OTBTemporalGapFilling                                                                                   
+      ln -sf ../../../../CESBIO/temporalgapfilling OTBTemporalGapFilling                                                                                   
       # Add iota2 module                                          
       echo "Adding iota2 module ..."                              
       cd $prefix_dir/CESBIO
-      #git clone http://tully.ups-tlse.fr/jordi/iota2.git
-      git clone https://framagit.org/inglada/iota2.git
+      if [ -d "./iota2" ]; then
+        echo "iota2 repository already cloned. skipping."
+      else
+        #git clone http://tully.ups-tlse.fr/jordi/iota2.git
+        git clone https://framagit.org/inglada/iota2.git
+      fi
       cd $prefix_dir/OTB/otb/Modules/Remote/
-      ln -s ../../../../CESBIO/iota2 
+      ln -sf ../../../../CESBIO/iota2 
     fi
   fi
   #----------------------------------------
@@ -101,8 +118,8 @@ if [[ "$ok" == "1" ]]; then
       mkdir -p build
       cd build
 
-      cmake -DCMAKE_CXX_FLAGS:STRING=-std=c++11 -DUSE_SYSTEM_BOOST=ON -DBUILD_TESTING=OFF -DCMAKE_BUILD_TYPE=Release -DOTB_WRAP_PYTHON:BOOL=ON -DGDAL_SB_EXTRA_OPTIONS:STRING="--with-python" -DCMAKE_INSTALL_PREFIX=$prefix_dir/OTB/install/ -DDOWNLOAD_LOCATION=$prefix_dir/OTB/SuperBuild-archives/ -DModule_IOTA2:BOOL=ON -DOTB_USE_QWT=ON -DOTB_USE_GLEW=ON -DOTB_USE_GLUT=ON -DOTB_USE_OPENGL=ON $prefix_dir/OTB/otb/SuperBuild/
-#      cmake -DCMAKE_CXX_FLAGS:STRING=-std=c++1y -DUSE_SYSTEM_BOOST=ON -DBUILD_TESTING=OFF -DCMAKE_BUILD_TYPE=Release -DOTB_WRAP_PYTHON:BOOL=ON -DGDAL_SB_EXTRA_OPTIONS:STRING="--with-python" -DCMAKE_INSTALL_PREFIX=$prefix_dir/OTB/install/ -DDOWNLOAD_LOCATION=$prefix_dir/OTB/SuperBuild-archives/ -DModule_IOTA2:BOOL=ON -DOTB_USE_QWT=ON -DOTB_USE_GLEW=ON -DOTB_USE_GLUT=ON -DOTB_USE_OPENGL=ON $prefix_dir/OTB/otb/SuperBuild/
+#     cmake -DCMAKE_CXX_FLAGS:STRING=-std=c++11 -DUSE_SYSTEM_BOOST=ON -DBUILD_TESTING=OFF -DCMAKE_BUILD_TYPE=Release -DOTB_WRAP_PYTHON:BOOL=ON -DGDAL_SB_EXTRA_OPTIONS:STRING="--with-python" -DCMAKE_INSTALL_PREFIX=$prefix_dir/OTB/install/ -DDOWNLOAD_LOCATION=$prefix_dir/OTB/SuperBuild-archives/ -DModule_IOTA2:BOOL=ON -DOTB_USE_QWT=ON -DOTB_USE_GLEW=ON -DOTB_USE_GLUT=ON -DOTB_USE_OPENGL=ON $prefix_dir/OTB/otb/SuperBuild/
+      cmake -DCMAKE_CXX_FLAGS:STRING=-std=c++1y -DUSE_SYSTEM_BOOST=ON -DBUILD_TESTING=OFF -DCMAKE_BUILD_TYPE=Release -DOTB_WRAP_PYTHON:BOOL=ON -DGDAL_SB_EXTRA_OPTIONS:STRING="--with-python" -DCMAKE_INSTALL_PREFIX=$prefix_dir/OTB/install/ -DDOWNLOAD_LOCATION=$prefix_dir/OTB/SuperBuild-archives/ -DModule_IOTA2:BOOL=ON -DOTB_USE_QWT=ON -DOTB_USE_GLEW=ON -DOTB_USE_GLUT=ON -DOTB_USE_OPENGL=ON $prefix_dir/OTB/otb/SuperBuild/
       make VERBOSE=1 -j2
     fi
     if [[ "$#" == 1 ]] || [[ "$2" == "iota2" ]]; then 
@@ -119,7 +136,7 @@ if [[ "$ok" == "1" ]]; then
   #----------------------------------------
   echo "Generate Archive ..."
   cd $prefix_dir
-  tar -czf iota2_OTB-6.2.tar.gz OTB/install CESBIO prepare_env* README*
-  echo "--> Archive ${prefix_dir}/iota2_OTB-6.0.tar.gz available"
+  tar -czf iota2_OTB-${OTB_VERSION}.tar.gz OTB/install CESBIO prepare_env* README*
+  echo "--> Archive ${prefix_dir}/iota2_OTB-${OTB_VERSION}.tar.gz available"
   echo "Generation process terminated"
 fi
