01bdf0c06f0821c282fbe0841b271d96b944a115
PENARD Cedric <cedric.penard@capgemini.com>
2018-05-03 13:29:41 +0000

    STYLE: cleaning fillVectorOGR.py

--- scripts/common/fillVectorOGR.py
+++ scripts/common/fillVectorOGR.py
@@ -14,51 +14,52 @@
 #
 # =========================================================================
 
-import argparse,os
-from osgeo import gdal
+import argparse
+import os
+#import random
+#from collections import defaultdict
+#from osgeo import gdal
 from osgeo import ogr
-from osgeo import osr
-from collections import defaultdict
+#from osgeo import osr
 import fileUtils as fu
-import random
 
 
-def extraction(vectorFill,vectorSource,field,field_val,driversFill,driversSource):
-	
-	ogrDriversSource = ogr.GetDriverByName(driversSource)
-	dataSourceSource = ogrDriversSource.Open(vectorSource, 0)
+def extraction(vectorFill, vectorSource, field, field_val, driversFill, driversSource):
 
-	layerSource = dataSourceSource.GetLayer()
+    ogrDriversSource = ogr.GetDriverByName(driversSource)
+    dataSourceSource = ogrDriversSource.Open(vectorSource, 0)
 
-	print "RECHERCHE DES FIDs"
-	All_FID=[(currentFeat.GetField(field),str(currentFeat.GetFID())) for currentFeat in layerSource if currentFeat.GetField(field) in field_val]
-	print "FIDs trouvée"
-	layerSource.ResetReading()
+    layerSource = dataSourceSource.GetLayer()
 
-	All_FID=fu.sortByFirstElem(All_FID)
+    print "RECHERCHE DES FIDs"
+    All_FID = [(currentFeat.GetField(field), str(currentFeat.GetFID())) for currentFeat in layerSource if currentFeat.GetField(field) in field_val]
+    print "FIDs trouvée"
+    layerSource.ResetReading()
 
-	for currentClass, FID in All_FID:
-		splits = fu.splitList(FID,len(vectorFill))
-		for currentSplit,currentVectorFill in zip(splits,vectorFill):
-			cmd = "ogr2ogr -append "+currentVectorFill+" "+vectorSource+" -where \" fid in ("+",".join(currentSplit)+")\""
-			print cmd 
-			print "Ajout de "+str(currentClass)+" dans "+currentVectorFill.split("/")[-1]
-			os.system(cmd)
+    All_FID = fu.sortByFirstElem(All_FID)
 
-#-where "fid in (2,0)"
+    for currentClass, FID in All_FID:
+        splits = fu.splitList(FID, len(vectorFill))
+        for currentSplit, currentVectorFill in zip(splits, vectorFill):
+            cmd = "ogr2ogr -append "+currentVectorFill+" "+vectorSource+" -where \" fid in ("+",".join(currentSplit)+")\""
+            print cmd
+            print "Ajout de "+str(currentClass)+" dans "+currentVectorFill.split("/")[-1]
+            os.system(cmd)
+
+#-where "fid in (2, 0)"
 if __name__ == "__main__":
 
-	parser = argparse.ArgumentParser(description = "all vector must have the same fields")
-	parser.add_argument("-vectorToFill",dest = "vectorFill",help ="vectors to fill up",default=None,required=True,nargs='+')
-	parser.add_argument("-vectorSource",dest = "vectorSource",help ="source vector",default=None,required=True)
-	parser.add_argument("-field",dest = "field",help ="data's field",default=None,required=True)
-	parser.add_argument("-field.value",dest = "field_val",help ="field's value",default=None,type = int,required=True,nargs='+')
-	parser.add_argument("-vectorToFill.driver",dest = "driversFill",help ="source's drivers",default=None,required=True,nargs='+')
-	parser.add_argument("-vectorSource.driver",dest = "driversSource",help ="source's drivers",default=None,required=True)
+    parser = argparse.ArgumentParser(description="all vector must have the same fields")
+    parser.add_argument("-vectorToFill", dest="vectorFill", help="vectors to fill up", default=None, required=True, nargs='+')
+    parser.add_argument("-vectorSource", dest="vectorSource", help="source vector", default=None, required=True)
+    parser.add_argument("-field", dest="field", help="data's field", default=None, required=True)
+    parser.add_argument("-field.value", dest="field_val", help="field's value", default=None, type=int, required=True, nargs='+')
+    parser.add_argument("-vectorToFill.driver", dest="driversFill", help="source's drivers", default=None, required=True, nargs='+')
+    parser.add_argument("-vectorSource.driver", dest="driversSource", help="source's drivers", default=None, required=True)
 
-	args = parser.parse_args()
+    args = parser.parse_args()
 
-	extraction(args.vectorFill,args.vectorSource,args.field,args.field_val,args.driversFill,args.driversSource)
+    extraction(args.vectorFill, args.vectorSource, args.field, args.field_val, args.driversFill, args.driversSource)
 
 #python fillVector.py -vectorSource.driver "ESRI Shapefile" -vectorToFill.driver "ESRI Shapefile" "ESRI Shapefile" -field.value 1 44 -field code -vectorSource /mnt/sdb1/Data/corse/test_sansForet.shp -vectorToFill /mnt/sdb1/Data/corse/test_avecForet_1.shp /mnt/sdb1/Data/corse/test_avecForet_2.shp
 
