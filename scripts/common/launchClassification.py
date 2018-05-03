c5e3ad5c7c0dd11dabc5a902827a0c91c59dd1cd
PENARD Cedric <cedric.penard@capgemini.com>
2018-05-03 09:55:52 +0000

    STYLE: cleaning launchClassification.py

--- scripts/common/launchClassification.py
+++ scripts/common/launchClassification.py
@@ -14,9 +14,11 @@
 #
 # =========================================================================
 
-import argparse,os,re
+import argparse
+import os
+import re
 from config import Config
-from osgeo import gdal, ogr,osr
+from osgeo import gdal, ogr, osr
 import fileUtils as fu
 import serviceConfigFile as SCF
 from Utils import run
@@ -24,7 +26,7 @@ from Utils import run
 def launchClassification(model, cfg, stat, pathToRT, pathToImg, pathToRegion,
                          fieldRegion, N, pathToCmdClassif, pathOut, pathWd):
 
-    if not isinstance(cfg,SCF.serviceConfigFile):
+    if not isinstance(cfg, SCF.serviceConfigFile):
         cfg = SCF.serviceConfigFile(cfg)
     pathConf = cfg.pathConf
     classif = cfg.getParam('argTrain', 'classifier')
@@ -44,38 +46,38 @@ def launchClassification(model, cfg, stat, pathToRT, pathToImg, pathToRegion,
     if not os.path.exists(maskFiles):
         run("mkdir "+maskFiles)
 
-    shpRName = pathToRegion.split("/")[-1].replace(".shp","")
+    shpRName = pathToRegion.split("/")[-1].replace(".shp", "")
 
-    AllModel_tmp = fu.FileSearch_AND(model,True,"model",".txt")
+    AllModel_tmp = fu.FileSearch_AND(model, True, "model", ".txt")
     AllModel = fu.fileSearchRegEx(model+"/*model*.txt")
 
     for currentFile in AllModel_tmp:
         if not currentFile in AllModel:
             os.remove(currentFile)
 
-    for path in AllModel :
+    for path in AllModel:
         model = path.split("/")[-1].split("_")[1]
-        tiles = fu.getListTileFromModel(model,outputPath+"/config_model/configModel.cfg")
+        tiles = fu.getListTileFromModel(model, outputPath+"/config_model/configModel.cfg")
         model_Mask = model
         if re.search('model_.*f.*_', path.split("/")[-1]):
             model_Mask = path.split("/")[-1].split("_")[1].split("f")[0]
-        seed = path.split("/")[-1].split("_")[-1].replace(".txt","")
+        seed = path.split("/")[-1].split("_")[-1].replace(".txt", "")
         tilesToEvaluate = tiles
-        if ("fusion" in classifMode and regionMode != "outside" ) or (regionMode == "one_region"):
+        if ("fusion" in classifMode and regionMode != "outside") or (regionMode == "one_region"):
             tilesToEvaluate = allTiles
         #construction du string de sortie
         for tile in tilesToEvaluate:
-            pathToFeat = fu.FileSearch_AND(pathToImg+"/"+tile+"/tmp/",True,".tif")[0]
+            pathToFeat = fu.FileSearch_AND(pathToImg+"/"+tile+"/tmp/", True, ".tif")[0]
             maskSHP = pathToRT+"/"+shpRName+"_region_"+model_Mask+"_"+tile+".shp"
             maskTif = shpRName+"_region_"+model_Mask+"_"+tile+".tif"
             CmdConfidenceMap = ""
             confidenceMap = ""
             if "fusion" in classifMode:
-                if regionMode!= "outside":
+                if regionMode != "outside":
                     tmp = pathOut.split("/")
-                    if pathOut[-1]=="/":
+                    if pathOut[-1] == "/":
                         del tmp[-1]
-                    tmp[-1]="envelope"
+                    tmp[-1] = "envelope"
                     pathToEnvelope = "/".join(tmp)
                     maskSHP = pathToEnvelope+"/"+tile+".shp"
             confidenceMap = tile+"_model_"+model+"_confidence_seed_"+seed+".tif"
@@ -86,7 +88,7 @@ def launchClassification(model, cfg, stat, pathToRT, pathToImg, pathToRegion,
                 #cas cluster
                 if pathWd != None:
                     maskFiles = pathWd
-                nameOut = fu.ClipVectorData(maskSHP,pathToMaskCommun, maskFiles,maskTif.replace(".tif",""))
+                nameOut = fu.ClipVectorData(maskSHP, pathToMaskCommun, maskFiles, maskTif.replace(".tif", ""))
                 cmdRaster = "otbcli_Rasterization -in "+nameOut+" -mode attribute -mode.attribute.field "+\
                         fieldRegion+" -im "+pathToFeat+" -out "+maskFiles+"/"+maskTif
                 if "fusion" in classifMode:
@@ -109,7 +111,7 @@ def launchClassification(model, cfg, stat, pathToRT, pathToImg, pathToRegion,
             pixType_cmd = " -pixType "+pixType
             cmdcpy = ""
             if pathWd != None:
-                pixType_cmd=pixType_cmd+" --wd $TMPDIR "
+                pixType_cmd = pixType_cmd+" --wd $TMPDIR "
             cmdcpy = ""
             cmd = appli+" -in "+pathToFeat+" -model "+path+" -mask "+pathOut+"/MASK/"+maskTif+" -out "+out+" "+pixType_cmd+" -ram 128 "+CmdConfidenceMap+" "+cmdcpy
 
@@ -118,24 +120,24 @@ def launchClassification(model, cfg, stat, pathToRT, pathToImg, pathToRegion,
                 cmd = cmd+" -imstat "+stat+"/Model_"+str(model)+".xml"
             AllCmd.append(cmd)
 
-    fu.writeCmds(pathToCmdClassif+"/class.txt",AllCmd)
+    fu.writeCmds(pathToCmdClassif+"/class.txt", AllCmd)
 
     return AllCmd
 
 if __name__ == "__main__":
 
-    parser = argparse.ArgumentParser(description = "This function allow you to create all classification command")
-    parser.add_argument("-path.model",help ="path to the folder which ONLY contains models for the classification(mandatory)",dest = "model",required=True)
-    parser.add_argument("-conf",help ="path to the configuration file which describe the learning method (mandatory)",dest = "pathConf",required=True)
-    parser.add_argument("--stat",dest = "stat",help ="statistics for classification",required=False)
-    parser.add_argument("-path.region.tile",dest = "pathToRT",help ="path to the folder which contains all region shapefile by tiles (mandatory)",required=True)
-    parser.add_argument("-path.img",dest = "pathToImg",help ="path where all images are stored",required=True)
-    parser.add_argument("-path.region",dest = "pathToRegion",help ="path to the global region shape",required=True)
-    parser.add_argument("-region.field",dest = "fieldRegion",help ="region field into region shape",required=True)
-    parser.add_argument("-N",dest = "N",help ="number of random sample(mandatory)",required=True)
-    parser.add_argument("-classif.out.cmd",dest = "pathToCmdClassif",help ="path where all classification cmd will be stored in a text file(mandatory)",required=True)
-    parser.add_argument("-out",dest = "pathOut",help ="path where to stock all classifications",required=True)
-    parser.add_argument("--wd",dest = "pathWd",help ="path to the working directory",default=None,required=False)
+    parser = argparse.ArgumentParser(description="This function allow you to create all classification command")
+    parser.add_argument("-path.model", help="path to the folder which ONLY contains models for the classification(mandatory)", dest="model", required=True)
+    parser.add_argument("-conf", help="path to the configuration file which describe the learning method (mandatory)", dest="pathConf", required=True)
+    parser.add_argument("--stat", dest="stat", help="statistics for classification", required=False)
+    parser.add_argument("-path.region.tile", dest="pathToRT", help="path to the folder which contains all region shapefile by tiles (mandatory)", required=True)
+    parser.add_argument("-path.img", dest="pathToImg", help="path where all images are stored", required=True)
+    parser.add_argument("-path.region", dest="pathToRegion", help="path to the global region shape", required=True)
+    parser.add_argument("-region.field", dest="fieldRegion", help="region field into region shape", required=True)
+    parser.add_argument("-N", dest="N", help="number of random sample(mandatory)", required=True)
+    parser.add_argument("-classif.out.cmd", dest="pathToCmdClassif", help="path where all classification cmd will be stored in a text file(mandatory)", required=True)
+    parser.add_argument("-out", dest="pathOut", help="path where to stock all classifications", required=True)
+    parser.add_argument("--wd", dest="pathWd", help="path to the working directory", default=None, required=False)
 
     args = parser.parse_args()
 
