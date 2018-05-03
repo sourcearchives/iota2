68a32716d3524cac2c4e78ded02351dda2923538
PENARD Cedric <cedric.penard@capgemini.com>
2018-05-03 13:27:59 +0000

    STYLE: cleaning genConfusionMatrix.py

--- scripts/common/genConfusionMatrix.py
+++ scripts/common/genConfusionMatrix.py
@@ -15,7 +15,9 @@
 # =========================================================================
 
 import argparse
-import sys,os,shutil
+import sys
+import os
+import shutil
 from config import Config
 import fileUtils as fu
 from osgeo import gdal
@@ -25,7 +27,7 @@ from Utils import run
 
 def create_dummy_rasters(missing_tiles, N, cfg):
     """
-    use when mode is 'one_region' but there is no validations / learning 
+    use when mode is 'one_region' but there is no validations / learning
     samples into a specific tile
     """
     #gdal_merge.py -n 0 -createonly -o Classif_T38JPT_model_1_seed_0_fake.tif Classif_T38JPT_model_1_seed_0.tif
@@ -40,11 +42,11 @@ def create_dummy_rasters(missing_tiles, N, cfg):
             dummy_raster_cmd = "gdal_merge.py -ot Byte -n 0 -createonly -o " + dummy_raster + " " + classif_tile
             run(dummy_raster_cmd)
 
-def compareRef(shapeRef,shapeLearn,classif,diff,footprint,workingDirectory, cfg, pathWd):
+def compareRef(shapeRef, shapeLearn, classif, diff, footprint, workingDirectory, cfg, pathWd):
 
-    minX,maxX,minY,maxY = fu.getRasterExtent(classif)
-    shapeRaster_val=workingDirectory+"/"+shapeRef.split("/")[-1].replace(".shp",".tif")
-    shapeRaster_learn=workingDirectory+"/"+shapeLearn.split("/")[-1].replace(".shp",".tif")
+    minX, maxX, minY, maxY = fu.getRasterExtent(classif)
+    shapeRaster_val = workingDirectory+"/"+shapeRef.split("/")[-1].replace(".shp", ".tif")
+    shapeRaster_learn = workingDirectory+"/"+shapeLearn.split("/")[-1].replace(".shp", ".tif")
 
     dataField = cfg.getParam('chain', 'dataField')
     spatialRes = int(cfg.getParam('chain', 'spatialResolution'))
@@ -57,13 +59,13 @@ def compareRef(shapeRef,shapeLearn,classif,diff,footprint,workingDirectory, cfg,
     run(cmd)
 
     #diff val
-    diff_val = workingDirectory+"/"+diff.split("/")[-1].replace(".tif","_val.tif")
+    diff_val = workingDirectory+"/"+diff.split("/")[-1].replace(".tif", "_val.tif")
     cmd_val = 'otbcli_BandMath -il '+shapeRaster_val+' '+classif+' -out '+diff_val+' uint8 -exp "im1b1==0?0:im1b1==im2b1?2:1"'#reference identique -> 2  | reference != -> 1 | pas de reference -> 0
     run(cmd_val)
     os.remove(shapeRaster_val)
 
     #diff learn
-    diff_learn = workingDirectory+"/"+diff.split("/")[-1].replace(".tif","_learn.tif")
+    diff_learn = workingDirectory+"/"+diff.split("/")[-1].replace(".tif", "_learn.tif")
     cmd_learn = 'otbcli_BandMath -il '+shapeRaster_learn+' '+classif+' -out '+diff_learn+' uint8 -exp "im1b1==0?0:im1b1==im2b1?4:3"'#reference identique -> 4  | reference != -> 3 | pas de reference -> 0
     run(cmd_learn)
     os.remove(shapeRaster_learn)
@@ -75,15 +77,15 @@ def compareRef(shapeRef,shapeLearn,classif,diff,footprint,workingDirectory, cfg,
     os.remove(diff_val)
     os.remove(diff_learn)
 
-    if pathWd and not os.path.exists (diff): 
-        shutil.copy(diff_tmp,diff)
+    if pathWd and not os.path.exists(diff):
+        shutil.copy(diff_tmp, diff)
         os.remove(diff_tmp)
 
     return diff
 
 def genConfMatrix(pathClassif, pathValid, N, dataField, pathToCmdConfusion,
                   cfg, pathWd):
-    if not isinstance(cfg,SCF.serviceConfigFile):
+    if not isinstance(cfg, SCF.serviceConfigFile):
         cfg = SCF.serviceConfigFile(cfg)
     AllCmd = []
     pathTMP = pathClassif+"/TMP"
@@ -96,7 +98,7 @@ def genConfMatrix(pathClassif, pathValid, N, dataField, pathToCmdConfusion,
         workingDirectory = pathWd
 
     AllTiles = []
-    validationFiles = fu.FileSearch_AND(pathValid,True,"_val.sqlite")
+    validationFiles = fu.FileSearch_AND(pathValid, True, "_val.sqlite")
     for valid in validationFiles:
         currentTile = valid.split("/")[-1].split("_")[0]
         try:
@@ -106,51 +108,51 @@ def genConfMatrix(pathClassif, pathValid, N, dataField, pathToCmdConfusion,
 
     for seed in range(N):
         #recherche de tout les shapeFiles par seed, par tuiles pour les fusionner
-        for tile in AllTiles:		
-            valTile = fu.FileSearch_AND(pathValid,True,tile,"_seed_"+str(seed)+"_val.sqlite")[0]
-            learnTile = fu.FileSearch_AND(pathValid,True,tile,"_seed_"+str(seed)+"_learn.sqlite")[0]
+        for tile in AllTiles:
+            valTile = fu.FileSearch_AND(pathValid, True, tile, "_seed_"+str(seed)+"_val.sqlite")[0]
+            learnTile = fu.FileSearch_AND(pathValid, True, tile, "_seed_"+str(seed)+"_learn.sqlite")[0]
             pathDirectory = pathTMP
             cmd = 'otbcli_ComputeConfusionMatrix -in {}/Classif_Seed_{}.tif -out {}/{}_seed_{}.csv -ref.vector.field {} -ref vector -ref.vector.in {}'.format(
-                                         pathClassif, seed, pathDirectory, tile, seed, dataField.lower(), valTile)
+                pathClassif, seed, pathDirectory, tile, seed, dataField.lower(), valTile)
             AllCmd.append(cmd)
             classif = pathTMP+"/"+tile+"_seed_"+str(seed)+".tif"
             diff = pathTMP+"/"+tile+"_seed_"+str(seed)+"_CompRef.tif"
-            footprint=pathTest+"/final/Classif_Seed_0.tif"
+            footprint = pathTest+"/final/Classif_Seed_0.tif"
             compareRef(valTile,
                        learnTile,
                        classif, diff, footprint, workingDirectory, cfg, pathWd)
 
-    fu.writeCmds(pathToCmdConfusion+"/confusion.txt",AllCmd)
+    fu.writeCmds(pathToCmdConfusion+"/confusion.txt", AllCmd)
 
     for seed in range(N):
-        AllDiff = fu.FileSearch_AND(pathTMP,True,"_seed_"+str(seed)+"_CompRef.tif")
+        AllDiff = fu.FileSearch_AND(pathTMP, True, "_seed_"+str(seed)+"_CompRef.tif")
         diff_seed = pathTest+"/final/diff_seed_"+str(seed)+".tif"
         if pathWd:
             diff_seed = workingDirectory+"/diff_seed_"+str(seed)+".tif"
-        fu.assembleTile_Merge(AllDiff,spatialRes,diff_seed,ot="Byte")
+        fu.assembleTile_Merge(AllDiff, spatialRes, diff_seed, ot="Byte")
         if pathWd:
-            shutil.copy(workingDirectory+"/diff_seed_"+str(seed)+".tif",pathTest+"/final/diff_seed_"+str(seed)+".tif")
+            shutil.copy(workingDirectory+"/diff_seed_"+str(seed)+".tif", pathTest+"/final/diff_seed_"+str(seed)+".tif")
 
     mode = cfg.getParam('chain', 'mode')
-    
+
     #Create dummy rasters if necessary
     tile_asked = cfg.getParam('chain', 'listTile').split()
     missing_tiles = [elem for elem in tile_asked if not elem in AllTiles]
     create_dummy_rasters(missing_tiles, N, cfg)
 
     return(AllCmd)
-    
+
 
 if __name__ == "__main__":
 
-    parser = argparse.ArgumentParser(description = "this function create a confusion matrix")
-    parser.add_argument("-path.classif",help ="path to the folder which contains classification images (mandatory)",dest = "pathClassif",required=True)
-    parser.add_argument("-path.valid",help ="path to the folder which contains validation samples (with priority) (mandatory)",dest = "pathValid",required=True)
-    parser.add_argument("-N",dest = "N",help ="number of random sample(mandatory)",required=True,type = int)
-    parser.add_argument("-data.field",dest = "dataField",help ="data's field into data shape (mandatory)",required=True)
-    parser.add_argument("-confusion.out.cmd",dest = "pathToCmdConfusion",help ="path where all confusion cmd will be stored in a text file(mandatory)",required=True)	
-    parser.add_argument("--wd",dest = "pathWd",help ="path to the working directory",default=None,required=False)
-    parser.add_argument("-conf",help ="path to the configuration file which describe the classification (mandatory)",dest = "pathConf",required=False)
+    parser = argparse.ArgumentParser(description="this function create a confusion matrix")
+    parser.add_argument("-path.classif", help="path to the folder which contains classification images (mandatory)", dest="pathClassif", required=True)
+    parser.add_argument("-path.valid", help="path to the folder which contains validation samples (with priority) (mandatory)", dest="pathValid", required=True)
+    parser.add_argument("-N", dest="N", help="number of random sample(mandatory)", required=True, type=int)
+    parser.add_argument("-data.field", dest="dataField", help="data's field into data shape (mandatory)", required=True)
+    parser.add_argument("-confusion.out.cmd", dest="pathToCmdConfusion", help="path where all confusion cmd will be stored in a text file(mandatory)", required=True)
+    parser.add_argument("--wd", dest="pathWd", help="path to the working directory", default=None, required=False)
+    parser.add_argument("-conf", help="path to the configuration file which describe the classification (mandatory)", dest="pathConf", required=False)
     args = parser.parse_args()
 
     # load configuration file
