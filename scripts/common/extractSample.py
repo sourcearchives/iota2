6216cddb827c1623179f592af3c9d8c937ff5c66
PENARD Cedric <cedric.penard@capgemini.com>
2018-05-03 14:31:28 +0000

    STYLE: cleaning extractSample.py

--- scripts/common/extractSample.py
+++ scripts/common/extractSample.py
@@ -14,101 +14,101 @@
 #
 # =========================================================================
 
-import argparse,os
+import argparse
+import os
+import random
+from collections import defaultdict
 from osgeo import gdal
 from osgeo import ogr
 from osgeo import osr
-from collections import defaultdict
 import fileUtils as fu
-import random
 
 
 def extraction(shapeE, DriverE, field, field_val, nb_extrac, shapeS, fieldo, DriverS):
-	
-	driver = ogr.GetDriverByName(DriverE)
-	dataSource = driver.Open(shapeE, 0)
-	layer = dataSource.GetLayer()
 
-        driver = ogr.GetDriverByName(DriverS)
-	dataSourceS = driver.Open(shapeS, 1)
-	layerS = dataSourceS.GetLayer()
+    driver = ogr.GetDriverByName(DriverE)
+    dataSource = driver.Open(shapeE, 0)
+    layer = dataSource.GetLayer()
 
-	print "checking FID"
-	All_FID=[(currentFeat.GetField(field),currentFeat.GetFID()) for currentFeat in layer if currentFeat.GetField(field) in field_val]
-	All_FID=fu.sortByFirstElem(All_FID)
-	print "FIDs found"
-        # get Fieldo index
-	
-	featureDefnS = layerS.GetLayerDefn()
-        indfieldo = featureDefnS.GetFieldIndex(fieldo)
+    driver = ogr.GetDriverByName(DriverS)
+    dataSourceS = driver.Open(shapeS, 1)
+    layerS = dataSourceS.GetLayer()
 
-        # Fields Lists
-        listFieldIn = fu.getAllFieldsInShape(shapeE, DriverE)        
-        listFieldOut = fu.getAllFieldsInShape(shapeS, DriverS)
+    print "checking FID"
+    All_FID = [(currentFeat.GetField(field), currentFeat.GetFID()) for currentFeat in layer if currentFeat.GetField(field) in field_val]
+    All_FID = fu.sortByFirstElem(All_FID)
+    print "FIDs found"
+    # get Fieldo index
 
-	numberOfFeatures = layerS.GetFIDColumn()
+    featureDefnS = layerS.GetLayerDefn()
+    indfieldo = featureDefnS.GetFieldIndex(fieldo)
 
-        # in case of not closed layers
-	
-        layerS.ResetReading()
-        layer.ResetReading()
+    # Fields Lists
+    listFieldIn = fu.getAllFieldsInShape(shapeE, DriverE)
+    listFieldOut = fu.getAllFieldsInShape(shapeS, DriverS)
 
-	i=0
-	fid_ind = layerS
-        for val in field_val:
-		print "fill up "+str(val)+" values"
-                # list of Fid of the current landcover type (val)
-                listFid = [x[1] for x in All_FID if x[0] == val][0]
-                # Random selection
-		print len(listFid)
-		nbExtraction =  nb_extrac[i]
-		if nbExtraction > len(listFid):
-			nbExtraction = len(listFid)
-			print "Warning : class "+str(val)+" extraction set to "+str(nbExtraction)
-                sublistFid = random.sample(listFid,nbExtraction)
-		
-		chunkSublistFID = fu.splitList(sublistFid,1+int(len(sublistFid)/1000))
-		filterFID = []
-		for chunk in chunkSublistFID:
-		        # Filter input shapefile
-			filterFID.append("("+" OR ".join([layer.GetFIDColumn()+"="+str(currentFID) for currentFID in chunk])+")")
-		
-		ffilter = " OR ".join(filterFID)
-		layer.SetAttributeFilter(ffilter)
-		newfid = max([feat.GetFID() for feat in layerS])
-		# filtered input features into output shapefile
-		for feature in layer:
-			geom = feature.GetGeometryRef()
-			dstfeature = ogr.Feature(layerS.GetLayerDefn())
-			dstfeature.SetGeometry(geom)
-			dstfeature.SetFID(newfid + 1)
-			newfid+=1
-			indIn = 0
-			while indIn < len(listFieldIn):
-				dstfeature.SetField(listFieldOut[indIn], feature.GetField(listFieldIn[indIn]))
-				indIn+=1
-			layerS.CreateFeature(dstfeature)
-			dstfeature.Destroy()
-                i += 1
+    numberOfFeatures = layerS.GetFIDColumn()
+
+    # in case of not closed layers
+
+    layerS.ResetReading()
+    layer.ResetReading()
+
+    i = 0
+    fid_ind = layerS
+    for val in field_val:
+        print "fill up "+str(val)+" values"
+        # list of Fid of the current landcover type (val)
+        listFid = [x[1] for x in All_FID if x[0] == val][0]
+        # Random selection
+        print len(listFid)
+        nbExtraction = nb_extrac[i]
+        if nbExtraction > len(listFid):
+            nbExtraction = len(listFid)
+            print "Warning : class "+str(val)+" extraction set to "+str(nbExtraction)
+            sublistFid = random.sample(listFid, nbExtraction)
+
+        chunkSublistFID = fu.splitList(sublistFid, 1+int(len(sublistFid)/1000))
+        filterFID = []
+        for chunk in chunkSublistFID:
+            # Filter input shapefile
+            filterFID.append("("+" OR ".join([layer.GetFIDColumn()+"="+str(currentFID) for currentFID in chunk])+")")
+
+        ffilter = " OR ".join(filterFID)
+        layer.SetAttributeFilter(ffilter)
+        newfid = max([feat.GetFID() for feat in layerS])
+        # filtered input features into output shapefile
+        for feature in layer:
+            geom = feature.GetGeometryRef()
+            dstfeature = ogr.Feature(layerS.GetLayerDefn())
+            dstfeature.SetGeometry(geom)
+            dstfeature.SetFID(newfid + 1)
+            newfid += 1
+            indIn = 0
+            while indIn < len(listFieldIn):
+                dstfeature.SetField(listFieldOut[indIn], feature.GetField(listFieldIn[indIn]))
+                indIn += 1
+            layerS.CreateFeature(dstfeature)
+            dstfeature.Destroy()
+        i += 1
 
         layerS = layer = None
-	
-	print "DONE"
-	
+
+    print "DONE"
+
 if __name__ == "__main__":
 
-	parser = argparse.ArgumentParser(description = "")
-	parser.add_argument("-in.extract",dest = "shapeE",help ="path to the folder which contains samples",default=None,required=True)
-	parser.add_argument("-in.extractDriver",dest = "DriverE",help ="driver",default=None,required=True)
-	parser.add_argument("-in.field",dest = "field",help ="data's field",default=None,required=True)
-	parser.add_argument("-in.field.value",dest = "field_val",help ="field's value",default=None,type = int,required=True,nargs='+')
-	parser.add_argument("-in.field.value.nbExtract",dest = "nbextract",help ="field's value",default=None,type = int,required=True,nargs='+')
-	parser.add_argument("-in.extract.select",dest = "shapeS",help ="path to the shapefile in which store samples",default=None,required=True)
-	parser.add_argument("-in.field.pop",dest = "fieldo",help ="Field of the storage shapefile to populate",default=None,required=True)
-	parser.add_argument("-in.PopDriver",dest = "DriverS",help ="driver",default=None,required=True)        
-	args = parser.parse_args()
+    parser = argparse.ArgumentParser(description="")
+    parser.add_argument("-in.extract", dest="shapeE", help="path to the folder which contains samples", default=None, required=True)
+    parser.add_argument("-in.extractDriver", dest="DriverE", help="driver", default=None, required=True)
+    parser.add_argument("-in.field", dest="field", help="data's field", default=None, required=True)
+    parser.add_argument("-in.field.value", dest="field_val", help="field's value", default=None, type=int, required=True, nargs='+')
+    parser.add_argument("-in.field.value.nbExtract", dest="nbextract", help="field's value", default=None, type=int, required=True, nargs='+')
+    parser.add_argument("-in.extract.select", dest="shapeS", help="path to the shapefile in which store samples", default=None, required=True)
+    parser.add_argument("-in.field.pop", dest="fieldo", help="Field of the storage shapefile to populate", default=None, required=True)
+    parser.add_argument("-in.PopDriver", dest="DriverS", help="driver", default=None, required=True)
+    args = parser.parse_args()
 
-	extraction(args.shapeE, args.DriverE, args.field, args.field_val, args.nbextract,args.shapeS,args.fieldo,args.DriverS)
+    extraction(args.shapeE, args.DriverE, args.field, args.field_val, args.nbextract, args.shapeS, args.fieldo, args.DriverS)
 
 #python extractSample.py -in.PopDriver SQLite -in.field.value.nbExtract 100 -in.field.value 42 -in.field CODE -in.extract /mnt/data/home/vincenta/tmp/testSampleExtraction/France_2014_1CA_T31TCJ_SamplesSel_5p.sqlite -in.extractDriver SQLite -in.extract.select /mnt/data/home/vincenta/tmp/testSampleExtraction/France_2014_1CA_T31TCJ_SamplesSel_20p.sqlite -in.field.pop code
-
