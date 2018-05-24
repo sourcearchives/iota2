e0e3d95c6639ccd558d9a04c3480ae64e23ccffe
Arthur VINCENT <arthur.vincent@outlook.fr>
2018-05-24 09:38:02 +0000

    STYLE : change directory's name samples to Sampling

--- scripts/common/iota2_builder.py
+++ scripts/common/iota2_builder.py
@@ -86,7 +86,7 @@ class iota2():
         import launchClassification as LC
         import ClassificationShaping as CS
         import genConfusionMatrix as GCM
-        from samples.DataAugmentation import augmentation_samples_user
+        from Sampling.DataAugmentation import augmentation_samples_user
         import ModelStat as MS
         import genResults as GR
         import os
