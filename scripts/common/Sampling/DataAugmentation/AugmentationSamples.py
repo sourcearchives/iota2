89f3c4120c747be8c0d8778fa23abca772cb4452
Arthur VINCENT <arthur.vincent@outlook.fr>
2018-05-25 13:57:18 +0000

    DOC : add AugmentationSamples.GetAugmentationSamplesParameters doc

--- scripts/common/Sampling/DataAugmentation/AugmentationSamples.py
+++ scripts/common/Sampling/DataAugmentation/AugmentationSamples.py
@@ -22,7 +22,33 @@ logger = logging.getLogger(__name__)
 
 
 def GetAugmentationSamplesParameters(IOTA2_dir):
-    """
+    """ read the /learningSample directory
+
+    parse the directory /learningSamples and return a list of all sqlite files
+
+    Parameter
+    ---------
+
+    IOTA2_dir : string
+        absolute path to the IOTA2's directory
+
+    Example
+    -------
+
+    ls /IOTA2/learningSamples
+    Samples_region_2_seed0_learn.sqlite
+    Samples_region_2_seed1_learn.sqlite
+    Samples_region_1_seed0_learn.sqlite
+    Samples_region_1_seed1_learn.sqlite
+
+    >>> GetAugmentationSamplesParameters("/IOTA2")
+        [[Samples_region_1_seed0_learn.sqlite, Samples_region_2_seed0_learn.sqlite],
+         [Samples_region_1_seed1_learn.sqlite, Samples_region_2_seed1_learn.sqlite]]
+
+    Return
+    ------
+    list
+        a list of sqlite files containing samples
     """
     IOTA2_dir_learningSamples = os.path.join(IOTA2_dir, "learningSamples")
     return fut.FileSearch_AND(IOTA2_dir_learningSamples, True, ".sqlite")
