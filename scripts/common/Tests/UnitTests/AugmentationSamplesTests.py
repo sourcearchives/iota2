0242849743297a6784646bea0b3103417f1ca79a
Arthur VINCENT <arthur.vincent@outlook.fr>
2018-05-28 15:22:37 +0000

    TEST : add AugmentationSamplesTests.test_iota2_augmentation test

--- scripts/common/Tests/UnitTests/AugmentationSamplesTests.py
+++ scripts/common/Tests/UnitTests/AugmentationSamplesTests.py
@@ -38,6 +38,7 @@ class iota_testSamplesAugmentation(unittest.TestCase):
         self.iota2_tests_directory = os.path.join(IOTA2DIR, "data", self.group_test_name)
         self.vector = os.path.join(IOTA2DIR, "data", "references", "sampler",
                                    "D0005H0002_polygons_To_Sample_Samples_ref_bindings.sqlite")
+        self.class_count = {51: 147, 11: 76, 12: 37, 42: 19}
         self.csvFile = os.path.join(IOTA2DIR, "data", "references", "sampleAugmentation.csv")
         self.all_tests_ok = []
         self.test_working_directory = None
@@ -65,6 +66,12 @@ class iota_testSamplesAugmentation(unittest.TestCase):
             shutil.rmtree(self.test_working_directory)
         os.mkdir(self.test_working_directory)
 
+        #Create test data
+        self.vector_test = os.path.join(self.test_working_directory, "vector_TEST_1_seed0.sqlite")
+        if os.path.exists(self.vector_test):
+            os.remove(self.vector_test)
+        shutil.copy(self.vector, self.vector_test)
+
     def list2reason(self, exc_list):
         if exc_list and exc_list[-1][0] is self:
             return exc_list[-1][1]
@@ -85,23 +92,41 @@ class iota_testSamplesAugmentation(unittest.TestCase):
         
         test the 3 differents strategies
         """
-        from collections import Counter
         self.test_working_directory
-        class_count = {51: 147, 11: 76, 12: 37, 42: 19}
+        
         balance_expected = {42: 128, 11: 71, 12: 110}
         atLeast_expected = {42: 101, 11: 44, 12: 83}
         byClass_expected = {42: 11, 51: 33, 12: 1}
-        class_augmentation_balance = AugmentationSamples.SamplesAugmentationCounter(class_count, mode="balance",
+        class_augmentation_balance = AugmentationSamples.SamplesAugmentationCounter(self.class_count, mode="balance",
                                                                                     atleast_num=None,
                                                                                     byclass=None)
         self.assertEqual(cmp(class_augmentation_balance, balance_expected), 0)
         
-        class_augmentation_atLeast = AugmentationSamples.SamplesAugmentationCounter(class_count, mode="atLeast",
+        class_augmentation_atLeast = AugmentationSamples.SamplesAugmentationCounter(self.class_count, mode="atLeast",
                                                                                     atleast_num=120,
                                                                                     byclass=None)
         self.assertEqual(cmp(class_augmentation_atLeast, atLeast_expected), 0)
         
-        class_augmentation_byClass = AugmentationSamples.SamplesAugmentationCounter(class_count, mode="byClass",
+        class_augmentation_byClass = AugmentationSamples.SamplesAugmentationCounter(self.class_count, mode="byClass",
                                                                                     atleast_num=None,
                                                                                     byclass=self.csvFile)
         self.assertEqual(cmp(class_augmentation_byClass, byClass_expected), 0)
+
+
+    def test_iota2_augmentation(self):
+        """Test sample augmentation workflow
+        """
+        from collections import Counter
+        class_augmentation_balance = AugmentationSamples.SamplesAugmentationCounter(self.class_count, mode="balance",
+                                                                                    atleast_num=None,
+                                                                                    byclass=None)
+        AugmentationSamples.Augmentation(self.vector_test, class_augmentation_balance,
+                                         strategy="jitter",
+                                         field="code", excluded_fields=[],
+                                         Jstdfactor=10,
+                                         Sneighbors=None,
+                                         workingDirectory=None)
+        class_count_test = Counter(fut.getFieldElement(self.vector_test, driverName="SQLite", field="code",
+                                                  mode="all", elemType="int"))
+        samples_number = self.class_count[max(self.class_count, key=lambda key: self.class_count[key])]
+        self.assertTrue(all([samples_number == v for k, v in class_count_test.items()]))
