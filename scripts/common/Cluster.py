88e5423a7085efcf8269afd9b80ae671157686bf
Arthur VINCENT <arthur.vincent@outlook.fr>
2018-06-05 13:27:56 +0000

    REFAC : move iota2.py to Iota2.py

--- scripts/common/Cluster.py
+++ scripts/common/Cluster.py
@@ -179,7 +179,7 @@ def write_PBS(job_directory, log_directory, task_name, step_to_compute,
     nprocs = int(MPI_process)*int(nb_chunk)
     
     exe = ("\nmpirun -x ITK_GLOBAL_DEFAULT_NUMBER_OF_THREADS={0} -np {1} "
-           "python {2}/iota2.py -config {3} "
+           "python {2}/Iota2.py -config {3} "
            "-starting_step {4} -ending_step {5} {6}").format(request.nb_cpu, nprocs,
                                                              script_path, config_path,
                                                              step_to_compute, step_to_compute,
