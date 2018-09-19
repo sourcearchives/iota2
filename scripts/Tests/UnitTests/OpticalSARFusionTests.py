2c7de747bf379710c6f6f84817e61ebf6da92ad6
Arthur VINCENT <arthur.vincent@outlook.fr>
2018-09-19 11:55:40 +0000

    TEST : add the script test OpticalSARFusionTests.py with skeleton framework

---
+++ scripts/Tests/UnitTests/OpticalSARFusionTests.py
@@ -0,0 +1,100 @@
+# !/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# =========================================================================
+#   Program:   iota2
+#
+#   Copyright (c) CESBIO. All rights reserved.
+#
+#   See LICENSE for details.
+#
+#   This software is distributed WITHOUT ANY WARRANTY; without even
+#   the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+#   PURPOSE.  See the above copyright notices for more information.
+#
+# =========================================================================
+
+# python -m unittest OpticalSARFusionTests
+
+import os
+import sys
+import shutil
+import unittest
+
+IOTA2DIR = os.environ.get('IOTA2DIR')
+
+if not IOTA2DIR:
+    raise Exception("IOTA2DIR environment variable must be set")
+
+# if all tests pass, remove 'iota2_tests_directory' which contains all
+# sub-directory tests
+RM_IF_ALL_OK = True
+
+IOTA2_SCRIPTS = IOTA2DIR + "/scripts"
+sys.path.append(IOTA2_SCRIPTS)
+
+from Common import FileUtils as fut
+
+
+class iota_testOpticalSARFusion(unittest.TestCase):
+    # before launching tests
+    @classmethod
+    def setUpClass(self):
+        # definition of local variables
+        self.group_test_name = "iota_testOpticalSARFusion"
+        self.iota2_tests_directory = os.path.join(IOTA2DIR, "data", self.group_test_name)
+        self.all_tests_ok = []
+
+        # References
+
+
+        # Tests directory
+        self.test_working_directory = None
+        if os.path.exists(self.iota2_tests_directory):
+            shutil.rmtree(self.iota2_tests_directory)
+        os.mkdir(self.iota2_tests_directory)
+
+    # after launching all tests
+    @classmethod
+    def tearDownClass(self):
+        print "{} ended".format(self.group_test_name)
+        if RM_IF_ALL_OK and all(self.all_tests_ok):
+            shutil.rmtree(self.iota2_tests_directory)
+
+    # before launching a test
+    def setUp(self):
+        """
+        create test environement (directories)
+        """
+        # self.test_working_directory is the diretory dedicated to each tests
+        # it changes for each tests
+
+        test_name = self.id().split(".")[-1]
+        self.test_working_directory = os.path.join(self.iota2_tests_directory, test_name)
+        if os.path.exists(self.test_working_directory):
+            shutil.rmtree(self.test_working_directory)
+        os.mkdir(self.test_working_directory)
+
+    def list2reason(self, exc_list):
+        if exc_list and exc_list[-1][0] is self:
+            return exc_list[-1][1]
+
+    # after launching a test, remove test's data if test succeed
+    def tearDown(self):
+        result = getattr(self, '_outcomeForDoCleanups', self._resultForDoCleanups)
+        error = self.list2reason(result.errors)
+        failure = self.list2reason(result.failures)
+        test_ok = not error and not failure
+        self.all_tests_ok.append(test_ok)
+        if test_ok:
+            shutil.rmtree(self.test_working_directory)
+
+    # Tests definitions
+    def test_1(self):
+        """
+        """
+        self.assertTrue(True)
+    def test_2(self):
+        """
+        """
+        self.assertTrue(True)
