aaba84f0f27a6e0d9091fe655db38fb69e9d0698
SPeillet <peillet.seb@gmail.com>
2018-11-13 14:19:58 +0100

    BUG: forget shutil library

--- scripts/Tests/UnitTests/CoRegistrationTests.py
+++ scripts/Tests/UnitTests/CoRegistrationTests.py
@@ -97,5 +97,5 @@ class iota_testCoRegistration(unittest.TestCase):
     		os.remove(file)
     	stackFiles = glob.glob(os.path.join(self.datadir,"T38KPD","*","*STACK*.tif"))
     	for file in stackFiles:
-    		shutil.remove(file)
+    		os.remove(file)
     		shutil.copy(os.path.join(os.path.dirname(file),"temp.tif"), file)
