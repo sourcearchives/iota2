68931e557f78e1ebce684f43b884dd3b6caf7fa9
SPeillet <peillet.seb@gmail.com>
2018-11-26 11:35:43 +0100

    ENH: add cascade coregistration mode

--- scripts/Common/Tools/CoRegister.py
+++ scripts/Common/Tools/CoRegister.py
@@ -51,6 +51,51 @@ def str2bool(v):
     else:
         raise argparse.ArgumentTypeError('Boolean value expected.')
 
+def get_S2_Tile_Coverage(file):
+    str=''
+    with open(file) as f:
+        for line in f:
+            str+=line
+            
+    band_str=str.split('<Band_Viewing_Incidence_Angles_Grids_List band_id="B2">')[-1].split('</Band_Viewing_Incidence_Angles_Grids_List>')[0]
+    detector_list=[]
+    for detector in band_str.split('<Values_List>')[1:]:
+        detector = detector.split('</Values_List>')[0]
+        tab = []
+        for line in detector.split('<VALUES>')[1:]:
+            line = line.split('</VALUES>')[0]
+            tab.append(line.split(' '))
+        detector_list.append(tab)
+
+    res_tab=[]
+    H=len(detector_list[0])
+    W=len(detector_list[0][0])
+    for i in range(0,H):
+        for j in range (0,W):
+            res=0
+            for array in detector_list:
+                if res == 0 and array[i][j] != 'NaN':
+                    res = 1
+            res_tab.append(res)
+    coverage_percent = float(sum(res_tab))/float(len(res_tab))
+    return coverage_percent
+
+def get_S2_Tile_Cloud_Cover(file):
+    with open(file) as f:
+        for line in f:
+            if 'name="CloudPercent"' in line :
+                cloud = float(line.split("</")[0].split('>')[1])
+                percent = 1-cloud/100
+    return percent
+
+def get_L8_Tile_Cloud_Cover(file):
+    with open(file) as f:
+        for line in f:
+            if 'CLOUD_COVER ' in line :
+                cloud = float(line.split(' = ')[1])
+                percent = 1-cloud /100
+    return percent
+
 def fitnessDateScore(dateVHR, datadir, datatype):
     """ get the date of the best image for the coregistration step
 
@@ -73,11 +118,7 @@ def fitnessDateScore(dateVHR, datadir, datatype):
             month = int(inDate[4:6])
             day = int(inDate[6:8])
             delta = 1 - min(abs((date(year,month,day) - dateVHR).days)/500,1)
-            with open(file) as f:
-                for line in f:
-                    if 'CLOUD_COVER ' in line :
-                        cloud = float(line.split(' = ')[1])
-                        percent = 1-cloud /100
+            percent = get_L8_Tile_Cloud_Cover(file)
             fitScore = delta*percent
             if maxFitScore < fitScore or maxFitScore is None:
                 maxFitScore = fitScore
@@ -91,17 +132,13 @@ def fitnessDateScore(dateVHR, datadir, datatype):
             month = int(inDate[4:6])
             day = int(inDate[6:8])
             delta = 1 - min(abs((date(year,month,day) - dateVHR).days)/500,1)
-            with open(file) as f:
-                for line in f:
-                    if 'name="CloudPercent"' in line :
-                        cloud = float(line.split("</")[0].split('>')[1])
-                        percent = 1-cloud/100
-            fitScore = delta*percent
+            percent = get_S2_Tile_Cloud_Cover(file)
+            cover = get_S2_Tile_Coverage(file)
+            fitScore = delta*percent*cover
             if maxFitScore < fitScore or maxFitScore is None:
                 maxFitScore = fitScore
                 fitDate = inDate
     return fitDate
-
 
 def launch_coregister(tile, cfg, workingDirectory):
     """ register an image / a time series on a reference image
@@ -135,19 +172,19 @@ def launch_coregister(tile, cfg, workingDirectory):
     ipathL5 = cfg.getParam('chain', 'L5Path')
     if ipathL5 != "None" and os.path.exists(os.path.join(ipathL5,tile)):
         datadir = os.path.join(ipathL5,tile)
-        datatype='L5'
+        datatype ='L5'
     ipathL8 = cfg.getParam('chain', 'L8Path')
     if ipathL8 != "None" and os.path.exists(os.path.join(ipathL8,tile)):
         datadir = os.path.join(ipathL8,tile)
-        datatype='L8'
+        datatype ='L8'
     ipathS2 = cfg.getParam('chain', 'S2Path')
     if ipathS2 != "None" and os.path.exists(os.path.join(ipathS2,tile)):
         datadir = os.path.join(ipathS2,tile)
-        datatype='S2'
+        datatype ='S2'
     ipathS2_S2C = cfg.getParam('chain', 'S2_S2C_Path')
     if ipathS2_S2C != "None" and os.path.exists(os.path.join(ipathS2_S2C,tile)):
         datadir = os.path.join(ipathS2_S2C,tile)
-        datatype='S2_S2C'
+        datatype ='S2_S2C'
 
     inref = os.path.join(cfg.getParam('coregistration','VHRPath'))
     datesSrc = cfg.getParam('coregistration', 'dateSrc')
@@ -157,13 +194,13 @@ def launch_coregister(tile, cfg, workingDirectory):
         dateSrc = datesSrc.split(" ")[tile_ind]
         if dateSrc == "None" :
             dateVHR = cfg.getParam('coregistration', 'dateVHR')
-            if dateVHR=='None':
+            if dateVHR =='None':
                 logger.warning("No dateVHR in configuration file, please fill dateVHR value")
             else :
                 dateSrc = fitnessDateScore(dateVHR,datadir,datatype)
     else:
         dateVHR = cfg.getParam('coregistration', 'dateVHR')
-        if dateVHR=='None':
+        if dateVHR =='None':
             logger.warning("No dateVHR in configuration file, please fill dateVHR value")
         else :
             dateSrc = fitnessDateScore(dateVHR,datadir,datatype)
@@ -178,9 +215,9 @@ def launch_coregister(tile, cfg, workingDirectory):
     prec = cfg.getParam('coregistration','prec')
     mode = cfg.getParam('coregistration','mode')
 
-    coregister(insrc, inref, bandsrc, bandref, resample, step, minstep, minsiftpoints, iterate, prec, mode, datadir, pattern, False)
+    coregister(insrc, inref, bandsrc, bandref, resample, step, minstep, minsiftpoints, iterate, prec, mode, datadir, pattern, datatype,False)
 
-def coregister(insrc, inref, band, bandref, resample=1, step=256, minstep=16, minsiftpoints=40, iterate=1, prec=3, mode=2, datadir=None, pattern='*STACK*', writeFeatures=False):
+def coregister(insrc, inref, band, bandref, resample=1, step=256, minstep=16, minsiftpoints=40, iterate=1, prec=3, mode=2, datadir=None, pattern='*STACK*', datatype='S2', writeFeatures=False):
     """ register an image / a time series on a reference image
 
     Parameters
@@ -246,7 +283,7 @@ def coregister(insrc, inref, band, bandref, resample=1, step=256, minstep=16, mi
     PMCMApp.ExecuteAndWriteOutput()
 
     # mode 1 : application on the source image
-    if mode == 1:
+    if mode == 1 or mode == 3:
         outSrc = str(pathWd + os.sep + 'temp_file.tif')
         io_Src = str(insrc + '?&skipcarto=true&geom=' + outSensorModel)
         ds = gdal.Open(insrc)
@@ -310,6 +347,203 @@ def coregister(insrc, inref, band, bandref, resample=1, step=256, minstep=16, mi
 
         if not writeFeatures and os.path.exists(outSensorModel):
             os.remove(outSensorModel)
+
+        if mode ==3:
+            folders = glob.glob(os.path.join(datadir,'*'))
+            dates = [os.path.basename(fld).split('_')[1].split("-")[0] for fld in folders]
+            ref_date = os.path.basename(insrc).split('_')[1].split("-")[0]
+            dates=dates.sort()
+            ref_date_ind = dates.index(ref_date)
+            bandref = band
+            clean_dates = [ref_date]
+            for date in reversed(dates[:ref_date_ind]):
+                inref = glob.glob(os.path.join(datadir,clean_dates[-1],pattern+'*.tif'))
+                insrc = glob.glob(os.path.join(datadir,date,pattern+'*.tif'))
+                pathWd = os.path.dirname(insrc)
+                outSensorModel = os.path.join(pathWd,'SensorModel.geom')
+                PMCMApp = OtbAppBank.CreatePointMatchCoregistrationModel({"in": insrc,
+                                                                        "band1": band,
+                                                                        "inref": inref,
+                                                                        "bandref": bandref,
+                                                                        "resample": resample,
+                                                                        "precision": str(prec),
+                                                                        "mfilter": "1",
+                                                                        "backmatching": "1",
+                                                                        "outgeom": outSensorModel,
+                                                                        "initgeobinstep": str(step),
+                                                                        "mingeobinstep": str(minstep),
+                                                                        "minsiftpoints": str(minsiftpoints),
+                                                                        "iterate": iterate
+                                                                        })
+                PMCMApp.ExecuteAndWriteOutput()
+
+                outSrc = str(pathWd + os.sep + 'temp_file.tif')
+                io_Src = str(insrc + '?&skipcarto=true&geom=' + outSensorModel)
+                ds = gdal.Open(insrc)
+                prj = ds.GetProjection()
+                gt = ds.GetGeoTransform()
+                srs = osr.SpatialReference()
+                srs.ImportFromWkt(prj)
+                code = srs.GetAuthorityCode(None)
+                gsp = str(int(2 * round(max(abs(gt[1]), abs(gt[5])))))
+                ds = None
+                orthoRecApp = OtbAppBank.CreateOrthoRectification({"in": io_Src,
+                                                                   "io.out": outSrc,
+                                                                   "map": "epsg",
+                                                                   "map.epsg.code": code,
+                                                                   "opt.gridspacing": gsp,
+                                                                   "pixType": "uint16"
+                                                                   })
+
+                if writeFeatures:
+                    orthoRecApp[0].ExecuteAndWriteOutput()
+                else:
+                    orthoRecApp[0].Execute()
+
+                ext = os.path.splitext(insrc)[1]
+                finalOutput = insrc.replace(ext, ext.replace('.', '_COREG.'))
+                superImposeApp = OtbAppBank.CreateSuperimposeApplication({"inr": insrc,
+                                                                        "inm": orthoRecApp[0],
+                                                                        "out": finalOutput,
+                                                                        "pixType": "uint16"})
+                superImposeApp[0].ExecuteAndWriteOutput()
+                os.remove(insrc)
+                shutil.move(finalOutput,insrc)
+
+                # Mask registration if exists
+                masks = glob.glob(os.path.dirname(insrc) + os.sep + 'MASKS' + os.sep + '*reproj' + ext)
+                if len(masks) != 0:
+                    for mask in masks:
+                        outSrc = str(os.path.dirname(insrc) + os.sep + 'temp_file.tif')
+                        io_Src = str(mask + '?&skipcarto=true&geom=' + outSensorModel)
+                        orthoRecApp = OtbAppBank.CreateOrthoRectification({"in": io_Src,
+                                                                           "io.out": outSrc,
+                                                                           "map": "epsg",
+                                                                           "map.epsg.code": code,
+                                                                           "opt.gridspacing": gsp,
+                                                                           "pixType": "uint16"
+                                                                           })
+                        if writeFeatures:
+                            orthoRecApp[0].ExecuteAndWriteOutput()
+                        else:
+                            orthoRecApp[0].Execute()
+
+                        ext = os.path.splitext(insrc)[1]
+                        finalmask = mask.replace(ext, ext.replace('.', '_COREG.'))
+                        superImposeApp= OtbAppBank.CreateSuperimposeApplication({"inr": mask,
+                                                                                "inm": orthoRecApp[0],
+                                                                                "out": finalmask,
+                                                                                "pixType": "uint16"})
+                        superImposeApp[0].ExecuteAndWriteOutput()
+                        os.remove(mask)
+                        shutil.move(finalmask,mask)
+                    
+                if datatype in in ['S2','S2_S2C']:
+                    mtd_file = glob.glob(os.path.join(pathWd,'*_MTD_ALL*'))[0]
+                    cloud_clear = get_S2_Tile_Cloud_Cover(mtd_file)
+                    cover = get_S2_Tile_Coverage(mtd_file)
+                    if cloud_clear > 0.6 and cover > 0.8
+                        clean_dates.append(date)
+                elif datatype in ['L5','L8']:
+                    mlt_file = glob.glob(os.path.join(pathWd,'*_MTL.txt*'))
+                    cloud_clear = get_L8_Tile_Cloud_Cover(mlt_file)
+                    if cloud_clear > 0.6 :
+                        clean_dates.append(date)
+
+            clean_dates = [ref_date]
+            for date in dates[ref_date_ind+1:]:
+                inref = glob.glob(os.path.join(datadir,clean_dates[-1],pattern+'*.tif'))
+                insrc = glob.glob(os.path.join(datadir,date,pattern+'*.tif'))
+                pathWd = os.path.dirname(insrc)
+                outSensorModel = os.path.join(pathWd,'SensorModel.geom')
+                PMCMApp = OtbAppBank.CreatePointMatchCoregistrationModel({"in": insrc,
+                                                                        "band1": band,
+                                                                        "inref": inref,
+                                                                        "bandref": bandref,
+                                                                        "resample": resample,
+                                                                        "precision": str(prec),
+                                                                        "mfilter": "1",
+                                                                        "backmatching": "1",
+                                                                        "outgeom": outSensorModel,
+                                                                        "initgeobinstep": str(step),
+                                                                        "mingeobinstep": str(minstep),
+                                                                        "minsiftpoints": str(minsiftpoints),
+                                                                        "iterate": iterate
+                                                                        })
+                PMCMApp.ExecuteAndWriteOutput()
+
+                outSrc = str(pathWd + os.sep + 'temp_file.tif')
+                io_Src = str(insrc + '?&skipcarto=true&geom=' + outSensorModel)
+                ds = gdal.Open(insrc)
+                prj = ds.GetProjection()
+                gt = ds.GetGeoTransform()
+                srs = osr.SpatialReference()
+                srs.ImportFromWkt(prj)
+                code = srs.GetAuthorityCode(None)
+                gsp = str(int(2 * round(max(abs(gt[1]), abs(gt[5])))))
+                ds = None
+                orthoRecApp = OtbAppBank.CreateOrthoRectification({"in": io_Src,
+                                                                   "io.out": outSrc,
+                                                                   "map": "epsg",
+                                                                   "map.epsg.code": code,
+                                                                   "opt.gridspacing": gsp,
+                                                                   "pixType": "uint16"
+                                                                   })
+
+                if writeFeatures:
+                    orthoRecApp[0].ExecuteAndWriteOutput()
+                else:
+                    orthoRecApp[0].Execute()
+
+                ext = os.path.splitext(insrc)[1]
+                finalOutput = insrc.replace(ext, ext.replace('.', '_COREG.'))
+                superImposeApp = OtbAppBank.CreateSuperimposeApplication({"inr": insrc,
+                                                                        "inm": orthoRecApp[0],
+                                                                        "out": finalOutput,
+                                                                        "pixType": "uint16"})
+                superImposeApp[0].ExecuteAndWriteOutput()
+                os.remove(insrc)
+                shutil.move(finalOutput,insrc)
+
+                # Mask registration if exists
+                masks = glob.glob(os.path.dirname(insrc) + os.sep + 'MASKS' + os.sep + '*reproj' + ext)
+                if len(masks) != 0:
+                    for mask in masks:
+                        outSrc = str(os.path.dirname(insrc) + os.sep + 'temp_file.tif')
+                        io_Src = str(mask + '?&skipcarto=true&geom=' + outSensorModel)
+                        orthoRecApp = OtbAppBank.CreateOrthoRectification({"in": io_Src,
+                                                                           "io.out": outSrc,
+                                                                           "map": "epsg",
+                                                                           "map.epsg.code": code,
+                                                                           "opt.gridspacing": gsp,
+                                                                           "pixType": "uint16"
+                                                                           })
+                        if writeFeatures:
+                            orthoRecApp[0].ExecuteAndWriteOutput()
+                        else:
+                            orthoRecApp[0].Execute()
+
+                        ext = os.path.splitext(insrc)[1]
+                        finalmask = mask.replace(ext, ext.replace('.', '_COREG.'))
+                        superImposeApp= OtbAppBank.CreateSuperimposeApplication({"inr": mask,
+                                                                                "inm": orthoRecApp[0],
+                                                                                "out": finalmask,
+                                                                                "pixType": "uint16"})
+                        superImposeApp[0].ExecuteAndWriteOutput()
+                        os.remove(mask)
+                        shutil.move(finalmask,mask)
+                    
+                if datatype in in ['S2','S2_S2C']:
+                    mtd_file = glob.glob(os.path.join(pathWd,'*_MTD_ALL*'))[0]
+                    cloud_clear = get_S2_Tile_Cloud_Cover(mtd_file)
+                    cover = get_S2_Tile_Coverage(mtd_file)
+                    if cloud_clear > 0.6 and cover > 0.8
+                        clean_dates.append(date)
+                elif datatype in ['L5','L8']:
+                    mlt_file = glob.glob(os.path.join(pathWd,'*_MTL.txt*'))
+                    cloud_clear = get_L8_Tile_Cloud_Cover(mlt_file)
+                    if cloud_clear > 0.6 :
+                        clean_dates.append(date)
 
     # mode 2 : application on the time series
     elif mode == 2:
