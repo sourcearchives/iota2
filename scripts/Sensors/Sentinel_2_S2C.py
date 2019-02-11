3ae1abf575f177a925885bf0199d68b5c6c7598a
Arthur VINCENT <arthur.vincent@outlook.fr>
2019-02-11 13:01:24 +0000

    ENH : Sentinel_2_S2C.preprocess() available

--- scripts/Sensors/Sentinel_2_S2C.py
+++ scripts/Sensors/Sentinel_2_S2C.py
@@ -72,15 +72,15 @@ class Sentinel_2_S2C(Sensor):
         # sensors attributes
         self.suffix = "STACK"
         self.masks_date_suffix = "BINARY_MASK"
+        self.scene_classif = "SCL_20m.jp2"
+        self.invalid_flags = [0, 1, 3, 8, 9, 10]
+        self.nodata_flag = 0
         self.date_position = 2# if date's name split by "_"
-        self.NODATA_VALUE = -10000
-        #~ self.masks_rules = OrderedDict({"CLM_R1.tif":0, "SAT_R1.tif":0, "EDG_R1.tif":0})# 0 mean data, else noData
-        #~ self.border_pos = 2
         self.features_names_list = ["NDVI", "NDWI", "Brightness"]
 
         # define bands to get and their order
-        self.stack_band_position = ["B2", "B3", "B4", "B5", "B6",
-                                    "B7", "B8", "B8A", "B11", "B12"]
+        self.stack_band_position = ["B02", "B03", "B04", "B05", "B06",
+                                    "B07", "B08", "B8A", "B11", "B12"]
         self.extracted_bands = None
         if extract_bands_flag:
             # TODO check every mandatory bands still selected -> def check_mandatory bands() return True/False
@@ -129,6 +129,11 @@ class Sentinel_2_S2C(Sensor):
         return sorted(dates_directories,
                       key=lambda x : int(os.path.basename(x).split("_")[self.date_position].split("T")[0]))
 
+    def get_date_from_name(self, product_name):
+        """
+        """
+        return product_name.split("_")[self.date_position].split("T")[0]
+
     def get_date_dir(self, date_dir, size):
         """
         """
@@ -141,20 +146,29 @@ class Sentinel_2_S2C(Sensor):
             raise Exception ("size not in [10, 20]")
         return target_dir
 
-    def build_stack_date_name(self, date_dir):
+    def build_date_name(self, date_dir, suffix):
         """
         """
         from Common.FileUtils import FileSearch_AND
         _, b2_name = os.path.split(FileSearch_AND(date_dir, True, "B02_10m.jp2")[0])
-        return b2_name.replace("B02_10m.jp2", "{}.tif".format(self.suffix))
+        return b2_name.replace("B02_10m.jp2", "{}.tif".format(suffix))
 
     def preprocess_date(self, date_dir, out_prepro, working_dir=None, ram=128,
                         logger=logger):
         """
         """
+        import shutil
+        from gdal import Warp
+        from osgeo.gdalconst import  GDT_Byte
+
         from Common.FileUtils import ensure_dir
+        from Common.FileUtils import FileSearch_AND
+        from Common.FileUtils import getRasterProjectionEPSG
+        from Common.OtbAppBank import CreateConcatenateImagesApplication
+        from Common.OtbAppBank import CreateSuperimposeApplication
+
         # manage directories
-        date_stack_name = self.build_stack_date_name(date_dir)
+        date_stack_name = self.build_date_name(date_dir, self.suffix)
         logger.debug("preprocessing {}".format(date_dir))
         r10_dir = self.get_date_dir(date_dir, 10)
 
@@ -168,6 +182,97 @@ class Sentinel_2_S2C(Sensor):
         if working_dir:
             out_stack_processing = os.path.join(working_dir, date_stack_name)
 
+        # get bands
+        date_bands = []
+        for band in self.stack_band_position:
+            if band in ["B02", "B03", "B04", "B08"]:
+                date_bands.append(FileSearch_AND(date_dir, True, "L2A", "{}_10m.jp2".format(band))[0])
+            elif band in ["B05", "B06", "B07", "B8A", "B11", "B12"]:
+                date_bands.append(FileSearch_AND(date_dir, True, "L2A", "{}_20m.jp2".format(band))[0])
+        # tile reference image generation
+        base_ref = date_bands[0]
+        logger.info("reference image generation {} from {}".format(self.ref_image, base_ref))
+        ensure_dir(os.path.dirname(self.ref_image), raise_exe=False)
+        base_ref_projection = getRasterProjectionEPSG(base_ref)
+        if not os.path.exists(self.ref_image):
+            ds = Warp(self.ref_image, base_ref, multithread=True,
+                      format="GTiff", xRes=10, yRes=10,
+                      outputType=GDT_Byte, srcSRS="EPSG:{}".format(base_ref_projection),
+                      dstSRS="EPSG:{}".format(self.target_proj))
+        # reproject / resample
+        bands_proj = OrderedDict()
+        all_reproj = []
+        for band, band_name in zip(date_bands, self.stack_band_position):
+            superimp, _ = CreateSuperimposeApplication({"inr": self.ref_image,
+                                                        "inm": band,
+                                                        "ram": str(ram)})
+            bands_proj[band_name] = superimp
+            all_reproj.append(superimp)
+
+        if not self.full_pipeline:
+            for reproj in all_reproj:
+                reproj.Execute()
+            date_stack = CreateConcatenateImagesApplication({"il": all_reproj,
+                                                             "ram": str(ram),
+                                                             "pixType" : "int16",
+                                                             "out": out_stack_processing})
+            if not os.path.exists(out_stack):
+                date_stack.ExecuteAndWriteOutput()
+                if working_dir:
+                    shutil.copy(out_stack_processing, out_stack)
+                    os.remove(out_stack_processing)
+        return bands_proj if self.full_pipeline else out_stack
+
+    def preprocess_date_masks(self, date_dir, out_prepro,
+                              working_dir=None, ram=128,
+                              logger=logger):
+        """
+        """
+        from gdal import Warp
+        import shutil
+        from Common.FileUtils import FileSearch_AND
+        from Common.FileUtils import ensure_dir
+        from Common.OtbAppBank import CreateBandMathApplication
+        from Common.OtbAppBank import CreateSuperimposeApplication
+        from Common.FileUtils import getRasterProjectionEPSG
+
+        # manage directories
+        date_mask_name = self.build_date_name(date_dir, self.masks_date_suffix)
+        logger.debug("preprocessing {}".format(date_dir))
+        r10_dir = self.get_date_dir(date_dir, 10)
+        out_mask = os.path.join(r10_dir, date_mask_name)
+        if out_prepro:
+            _, date_dir_name = os.path.split(date_dir)
+            out_dir = r10_dir.replace(date_dir, out_prepro)
+            ensure_dir(out_dir, raise_exe=False)
+            out_mask = os.path.join(out_dir, date_mask_name)
+        out_mask_processing = out_mask
+        if working_dir:
+            out_mask_processing = os.path.join(working_dir, date_mask_name)
+
+        r20m_dir = self.get_date_dir(date_dir, 20)
+        scl = FileSearch_AND(r20m_dir, True, self.scene_classif)[0]
+        invalid_expr = " or ".join(["im1b1=={}".format(flag) for flag in self.invalid_flags])
+        binary_mask = CreateBandMathApplication({"il": scl,
+                                                 "exp": "{}?1:0".format(invalid_expr),
+                                                 "pixType" : "uint8"})
+        binary_mask.Execute()
+        app_dep= [binary_mask]
+
+        superimp, _ = CreateSuperimposeApplication({"inr": self.ref_image,
+                                                    "inm": binary_mask,
+                                                    "out": out_mask_processing,
+                                                    "pixType":"uint8",
+                                                    "ram": str(ram)})
+        if not self.full_pipeline:
+            if not os.path.exists(out_mask):
+                superimp.ExecuteAndWriteOutput()
+                if working_dir:
+                    shutil.copy(out_mask_processing, out_mask)
+                    os.remove(out_mask_processing)
+
+        return superimp, app_dep
+
     def preprocess(self, working_dir=None, ram=128, logger=logger):
         """
         """       
@@ -178,13 +283,13 @@ class Sentinel_2_S2C(Sensor):
         for date in input_dates:
             data_prepro = self.preprocess_date(date, self.output_preprocess_directory,
                                                working_dir, ram)
-            #~ data_mask = self.preprocess_date_masks(date, self.output_preprocess_directory,
-                                                   #~ working_dir, ram)
-            #~ current_date = self.get_date_from_name(os.path.basename(date))
+            data_mask = self.preprocess_date_masks(date, self.output_preprocess_directory,
+                                                   working_dir, ram)
+            current_date = self.get_date_from_name(os.path.basename(date))
             # TODO check if current_date already exists
-            #~ preprocessed_dates[current_date] = {"data": data_prepro,
-                                                #~ "mask": data_mask}
-        #~ return preprocessed_dates
+            preprocessed_dates[current_date] = {"data": data_prepro,
+                                                "mask": data_mask}
+        return preprocessed_dates
 
     def footprint(self, ram=128):
         """
