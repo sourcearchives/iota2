971edf45ac864534bd1e64fcc546c0c68a8712fb
Arthur VINCENT <arthur.vincent@outlook.fr>
2019-02-11 14:36:43 +0000

    ENH : Sentinel_2_S2C.get_time_series() available

--- scripts/Sensors/Sentinel_2_S2C.py
+++ scripts/Sensors/Sentinel_2_S2C.py
@@ -110,18 +110,6 @@ class Sentinel_2_S2C(Sensor):
                                                            tile_name)
         self.interpolated_dates = "{}_{}_interpolation_dates.txt".format(self.__class__.name,
                                                                          tile_name)
-        
-    def get_available_dates(self):
-        """
-        return sorted available dates
-        """
-        pass
-
-    def get_available_dates_masks(self):
-        """
-        return sorted available masks
-        """
-        pass
 
     def sort_dates_directories(self, dates_directories):
         """
@@ -326,10 +314,124 @@ class Sentinel_2_S2C(Sensor):
 
         return superimp, app_dep
 
+    def get_available_dates(self):
+        """
+        return sorted available dates
+        """
+        from Common.FileUtils import FileSearch_AND
+        target_folder = self.tile_directory
+        if self.output_preprocess_directory:
+            target_folder = self.output_preprocess_directory
+        
+        stacks = sorted(FileSearch_AND(target_folder, True, "{}.tif".format(self.suffix)),
+                        key=lambda x : os.path.basename(x).split("_")[self.date_position].split("T")[0])
+        return stacks
+
+    def get_available_dates_masks(self):
+        """
+        return sorted available masks
+        """
+        from Common.FileUtils import FileSearch_AND
+        target_folder = self.tile_directory
+        if self.output_preprocess_directory:
+            target_folder = self.output_preprocess_directory
+        masks = sorted(FileSearch_AND(target_folder, True, "{}.tif".format(self.masks_date_suffix)),
+                       key=lambda x : os.path.basename(x).split("_")[self.date_position].split("T")[0])
+        return masks
+
+    def write_dates_file(self):
+        """
+        """
+        from Common.FileUtils import ensure_dir
+        input_dates_dir = [os.path.join(self.tile_directory, cdir) for cdir in os.listdir(self.tile_directory)]
+        date_file = os.path.join(self.features_dir, "tmp", self.input_dates)
+        all_available_dates = [os.path.basename(date).split("_")[self.date_position].split("T")[0] for date in input_dates_dir]
+        all_available_dates = sorted(all_available_dates, key=lambda x:int(x))
+        if not os.path.exists(date_file):
+            with open(date_file, "w") as input_date_file:
+                input_date_file.write("\n".join(all_available_dates))
+        return date_file, all_available_dates
+
     def get_time_series(self, ram=128):
         """
+        TODO : be able of using a date interval
+        Return
+        ------
+            list
+                [(otb_Application, some otb's objects), time_series_labels]
+                Functions dealing with otb's application instance has to 
+                returns every objects in the pipeline
         """
-        pass
+        from Common.OtbAppBank import CreateConcatenateImagesApplication
+        from Common.FileUtils import ensure_dir
+
+        # needed to travel throught iota2's library
+        app_dep = []
+
+        preprocessed_dates = self.preprocess(working_dir=None, ram=str(ram))
+
+        if self.full_pipeline:
+            dates_concatenation = []
+            for date, dico_date in preprocessed_dates.items():
+                for band_name, reproj_date in dico_date["data"].items():
+                    dates_concatenation.append(reproj_date)
+                    reproj_date.Execute()
+                    app_dep.append(reproj_date)
+        else:
+            dates_concatenation = self.get_available_dates()
+
+        time_series_dir = os.path.join(self.features_dir, "tmp")
+        ensure_dir(time_series_dir, raise_exe=False)
+        times_series_raster = os.path.join(time_series_dir, self.time_series_name)
+
+        dates_time_series = CreateConcatenateImagesApplication({"il": dates_concatenation,
+                                                                "out": times_series_raster,
+                                                                "pixType": "int16",
+                                                                "ram": str(ram)})
+        dates_in_file, dates_in = self.write_dates_file()
+
+        # build labels
+        features_labels = ["{}_{}_{}".format(self.__class__.name, band_name, date) for date in dates_in for band_name in self.stack_band_position]
+        
+        # if not all bands must be used
+        if self.extracted_bands:
+            app_dep.append(dates_time_series)
+            (dates_time_series,
+             features_labels) = self.extract_bands_time_series(dates_time_series,
+                                                               dates_in,
+                                                               len(self.stack_band_position),
+                                                               self.extracted_bands,
+                                                               ram)
+        return (dates_time_series, app_dep), features_labels
+
+    def extract_bands_time_series(self, dates_time_series,
+                                  dates_in,
+                                  comp,
+                                  extract_bands,
+                                  ram):
+        """
+        TODO : mv to base class ?
+        extract_bands : list
+             [('bandName', band_position), ...]
+        comp : number of bands in original stack
+        """
+        from Common.OtbAppBank import CreateExtractROIApplication
+
+        nb_dates = len(dates_in)
+        channels_interest = []
+        for date_number in range(nb_dates):
+            for band_name, band_position in extract_bands:
+                channels_interest.append(band_position + int(date_number * comp))
+
+        features_labels = ["{}_{}_{}".format(self.__class__.name, band_name, date) for date in dates_in for band_name, band_pos in extract_bands]
+        channels_list = ["Channel{}".format(channel) for channel in channels_interest]
+        time_series_out = dates_time_series.GetParameterString("out")
+        dates_time_series.Execute()
+        extract = CreateExtractROIApplication({"in":dates_time_series,
+                                               "cl":channels_list,
+                                               "ram":str(ram),
+                                               "out":dates_time_series.GetParameterString("out")})
+        return extract, features_labels
 
     def get_time_series_masks(self, ram=128, logger=logger):
         """
